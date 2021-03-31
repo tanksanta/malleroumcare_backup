eb242a667d64b769583632c1d5359e58303d4b40
오성훈 <oseonghun@oseonghun-ui-MacBookPro.local>
2021-03-31 19:16:05 +0900

    주문상세수정

--- www/shop/orderinquiryview.php
+++ www/shop/orderinquiryview.php
@@ -569,7 +569,6 @@ if($is_inquiryview_sub) {
                 $res_pen = json_decode($res_pen, true);
                 curl_close($oCurl);
                 $data = $res_pen["data"][0];
-                print_r($res2);
                 //수급자명
                 $ren_person=$data["penNm"];
                 $stoId=$res2['data'][0]['stoId'];
@@ -669,16 +668,16 @@ if($is_inquiryview_sub) {
 		$sendData["prods"] = $_SESSION["productList{$_GET["od_id"]}"];
 		$sendData["staOrdCd"] = $staOrdCd;
 
-		// unset($_SESSION["productList{$_GET["od_id"]}"]);
-		// unset($_SESSION["deliveryTotalCnt{$_GET["od_id"]}"]);
-		// unset($_SESSION["uuid{$_GET["od_id"]}"]);
-		// unset($_SESSION["penOrdId{$_GET["od_id"]}"]);
+		unset($_SESSION["productList{$_GET["od_id"]}"]);
+		unset($_SESSION["deliveryTotalCnt{$_GET["od_id"]}"]);
+		unset($_SESSION["uuid{$_GET["od_id"]}"]);
+		unset($_SESSION["penOrdId{$_GET["od_id"]}"]);
 
 ?>
 
 	<script type="text/javascript">
 		<?php if($reload){ ?>
-			// window.location.reload();
+			window.location.reload();
 		<?php } ?>
 		var sendData = <?=json_encode($sendData, JSON_UNESCAPED_UNICODE)?>;
 
