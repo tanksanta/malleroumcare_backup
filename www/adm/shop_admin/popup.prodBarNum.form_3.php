709b72db2c50b0a58e957bf4b669f5bb28cba8cf
오성훈 <oseonghun@oseonghun-ui-MacBookPro.local>
2021-04-06 14:46:29 +0900

    주문폼 바코드 placrholder 수정

--- www/adm/shop_admin/popup.prodBarNum.form_3.php
+++ www/adm/shop_admin/popup.prodBarNum.form_3.php
@@ -379,7 +379,7 @@
 									<ul class="inputbox">
 										<?php for($b = 0; $b< $options[$k]["ct_qty"]; $b++){ ?>
 											<li>
-												<input type="number" readonly maxlength="12" oninput="maxLengthCheck(this)" value="<?=$prodList[$b]["prodBarNum"]?>"class="notall frm_input frm_input_<?=$prodListCnt?> required prodBarNumItem_<?=$prodList[$prodListCnt]["penStaSeq"]?> <?=$stoIdDataList[$prodListCnt]?>" placeholder="바코드를 입력하세요." data-frm-no="<?=$prodListCnt?>" maxlength="12">
+												<input type="number" readonly maxlength="12" oninput="maxLengthCheck(this)" value="<?=$prodList[$b]["prodBarNum"]?>"class="notall frm_input frm_input_<?=$prodListCnt?> required prodBarNumItem_<?=$prodList[$prodListCnt]["penStaSeq"]?> <?=$stoIdDataList[$prodListCnt]?>" placeholder="바코드가 입력되지 않았습니다." data-frm-no="<?=$prodListCnt?>" maxlength="12">
 												<!-- <i class="fa fa-check"></i> -->
 												<!-- <span class="overlap">중복</span> -->
 												<!-- <img src="<?php echo G5_IMG_URL?>/bacod_img.png" class="nativePopupOpenBtn" data-code="<?=$b?>"> -->
