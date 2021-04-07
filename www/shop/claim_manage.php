c5a7cff01ca401178a4f13026f5a0bbfcc322c9a
오성훈 <oseonghun@oseonghun-ui-MacBookPro.local>
2021-04-07 16:01:19 +0900

    청구, 전자문서

---
+++ www/shop/claim_manage.php
@@ -0,0 +1,93 @@
+<?php
+include_once('./_common.php');
+
+if(USE_G5_THEME && defined('G5_THEME_PATH')) {
+    require_once(G5_SHOP_PATH.'/yc/orderinquiry.php');
+    return;
+}
+
+define("_ORDERINQUIRY_", true);
+
+$od_pwd = get_encrypt_string($od_pwd);
+
+// 회원인 경우
+if ($is_member)
+{
+    $sql_common = " from {$g5['g5_shop_order_table']} where mb_id = '{$member['mb_id']}' AND od_del_yn = 'N' ";
+}
+else if ($od_id && $od_pwd) // 비회원인 경우 주문서번호와 비밀번호가 넘어왔다면
+{
+    $sql_common = " from {$g5['g5_shop_order_table']} where od_id = '$od_id' and od_pwd = '$od_pwd' AND od_del_yn = 'N' ";
+}
+else // 그렇지 않다면 로그인으로 가기
+{
+    goto_url(G5_BBS_URL.'/login.php?url='.urlencode(G5_SHOP_URL.'/orderinquiry.php'));
+}
+
+// Page ID
+$pid = ($pid) ? $pid : 'inquiry';
+$at = apms_page_thema($pid);
+include_once(G5_LIB_PATH.'/apms.thema.lib.php');
+
+$skin_row = array();
+$skin_row = apms_rows('order_'.MOBILE_.'skin, order_'.MOBILE_.'set');
+$skin_name = $skin_row['order_'.MOBILE_.'skin'];
+$order_skin_path = G5_SKIN_PATH.'/apms/order/'.$skin_name;
+$order_skin_url = G5_SKIN_URL.'/apms/order/'.$skin_name;
+
+// 스킨 체크
+list($order_skin_path, $order_skin_url) = apms_skin_thema('shop/order', $order_skin_path, $order_skin_url);
+
+// 스킨설정
+$wset = array();
+if($skin_row['order_'.MOBILE_.'set']) {
+	$wset = apms_unpack($skin_row['order_'.MOBILE_.'set']);
+}
+
+// 데모
+if($is_demo) {
+	@include ($demo_setup_file);
+}
+
+// 설정값 불러오기
+$is_inquiry_sub = false;
+@include_once($order_skin_path.'/config.skin.php');
+
+$g5['title'] = '주문내역조회';
+
+if($is_inquiry_sub) {
+	include_once(G5_PATH.'/head.sub.php');
+	if(!USE_G5_THEME) @include_once(THEMA_PATH.'/head.sub.php');
+} else {
+	include_once('./_head.php');
+}
+
+$skin_path = $order_skin_path;
+$skin_url = $order_skin_url;
+
+// 셋업
+$setup_href = '';
+if(is_file($skin_path.'/setup.skin.php') && ($is_demo || $is_designer)) {
+	$setup_href = './skin.setup.php?skin=order&amp;name='.urlencode($skin_name).'&amp;ts='.urlencode(THEMA);
+}
+
+
+?>
+
+<!-- 내용 -->
+<section>
+
+</section>
+
+
+
+
+
+<?php
+if($is_inquiry_sub) {
+	if(!USE_G5_THEME) @include_once(THEMA_PATH.'/tail.sub.php');
+	include_once(G5_PATH.'/tail.sub.php');
+} else {
+	include_once('./_tail.php');
+}
+?>
