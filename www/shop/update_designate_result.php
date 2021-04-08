778704205c8925eb9dba4744712f9ac75d91c9c0
오성훈 <oseonghun@oseonghun-ui-MacBookPro.local>
2021-04-08 14:48:42 +0900

    1_11. 소독관련 필수값 수정

--- www/shop/update_designate_result.php
+++ www/shop/update_designate_result.php
@@ -1,39 +1,45 @@
 <?php
     include_once('./_common.php');
-    $max_file_size = 2097152;
-    // 변수 정리
-    $uploads_dir = G5_DATA_PATH.'/file/disinfection';
-    $error = $_FILES['dis_file']['error'];
-    $name = $_FILES['dis_file']['name'];
-    $allowed_ext = array('exe');
-    $ext = array_pop(explode('.', $name));
-    $temp = explode(".", $_FILES["dis_file"]["name"]);
-    $newfilename = $_POST['member'].'_'.round(microtime(true)) . '.' . end($temp);
-    // 오류 확인
-    if( $error != UPLOAD_ERR_OK ) {
-        switch( $error ) {
-            case UPLOAD_ERR_INI_SIZE:
-            case UPLOAD_ERR_FORM_SIZE:
-                echo "파일이 너무 큽니다. ($error)";
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                echo "파일이 첨부되지 않았습니다. ($error)";
-                break;
-            default:
-                echo "파일이 제대로 업로드되지 않았습니다. ($error)";
-        }
-        exit;
-    }
-    if($file['size'] >= $max_file_size) {
-        alert("2MB 까지만 업로드 가능합니다.");
-        return false;
-    }
-    // 확장자 확인
-    if( in_array($ext, $allowed_ext) ) {
-        echo "허용되지 않는 확장자입니다.";
-        exit;
-    }
 
+
+    if($_FILES['dis_file']['tmp_name']){
+        $max_file_size = 2097152;
+        // 변수 정리
+        $uploads_dir = G5_DATA_PATH.'/file/disinfection';
+        $error = $_FILES['dis_file']['error'];
+        $name = $_FILES['dis_file']['name'];
+        $allowed_ext = array('exe');
+        $ext = array_pop(explode('.', $name));
+        $temp = explode(".", $_FILES["dis_file"]["name"]);
+        $newfilename = $_POST['member'].'_'.round(microtime(true)) . '.' . end($temp);
+    
+        // 오류 확인
+        if( $error != UPLOAD_ERR_OK ) {
+            switch( $error ) {
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    alert('파일이 너무 큽니다.');
+                    break;
+                // case UPLOAD_ERR_NO_FILE:
+                //     echo "파일이 첨부되지 않았습니다. ($error)";
+                //     break;
+                default:
+                    alert('파일이 제대로 업로드되지 않았습니다.');
+            }
+            exit;
+        }
+        if($file['size'] >= $max_file_size) {
+            alert("2MB 까지만 업로드 가능합니다.");
+            return false;
+        }
+        // 확장자 확인
+        if( in_array($ext, $allowed_ext) ) {
+            alert('허용되지 않는 확장자입니다.');
+            exit;
+        }
+        //파일저장
+        move_uploaded_file( $_FILES['dis_file']['tmp_name'], "$uploads_dir/$newfilename");
+    }
     //최신 소독로그 조회
     $sql = "SELECT `rental_log_Id` FROM `g5_rental_log` WHERE `stoId`= '{$stoId}' AND `rental_log_division`='1' ORDER BY `dis_total_date` DESC LIMIT 1";
     // echo $sql;
@@ -49,8 +55,9 @@
             `dis_file` = '{$newfilename}'
             where `rental_log_Id` = '{$rental_log_Id}'";
     sql_query($sql_update);
-    //파일저장
-    move_uploaded_file( $_FILES['dis_file']['tmp_name'], "$uploads_dir/$newfilename");
+
+
+
     //대여가능 변경
     $sendData=[];
     $prodsSendData = [];
