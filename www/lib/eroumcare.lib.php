41440252647611c43da2c5ce7531e69387d25af1
liam <pelogvc@gmail.com>
2021-04-17 13:58:38 +0900

    fix: 상품 목록 가져오는 함수 추가

--- www/lib/eroumcare.lib.php
+++ www/lib/eroumcare.lib.php
@@ -14,3 +14,123 @@ function get_eroumcare($api_url, $data) {
     
 	return $res;
 }
+
+function get_carts_by_od_id($od_id, $delivery_yn = null) {
+
+	if ($delivery_yn) {
+		$delivery_where = "AND a.ct_delivery_yn = '". $delivery_yn ."'";
+	}
+
+	global $g5;
+	
+	// 상품목록
+	$sql = " select a.ct_id,
+					a.it_id,
+					a.it_name,
+					a.cp_price,
+					a.ct_notax,
+					a.ct_send_cost,
+					a.ct_sendcost,
+					a.it_sc_type,
+					a.pt_it,
+					a.pt_id,
+					b.ca_id,
+					b.ca_id2,
+					b.ca_id3,
+					b.pt_msg1,
+					b.pt_msg2,
+					b.pt_msg3,
+					a.ct_status,
+					b.it_model,
+					b.it_outsourcing_use,
+					b.it_outsourcing_company,
+					b.it_outsourcing_manager,
+					b.it_outsourcing_email,
+					b.it_outsourcing_option,
+					b.it_outsourcing_option2,
+					b.it_outsourcing_option3,
+					b.it_outsourcing_option4,
+					b.it_outsourcing_option5,
+					a.pt_old_name,
+					a.pt_old_opt,
+					a.ct_uid,
+					a.prodMemo,
+					a.prodSupYn,
+					a.ct_qty,
+					a.ct_stock_qty,
+					b.it_img1,
+					a.ct_delivery_company,
+					a.ct_delivery_num,
+					a.ct_combine_ct_id
+			  from {$g5['g5_shop_cart_table']} a left join {$g5['g5_shop_item_table']} b on ( a.it_id = b.it_id )
+			  where a.od_id = '$od_id'
+			  $delivery_where
+			  group by a.it_id, a.ct_uid
+			  order by a.ct_id ";
+
+	$result = sql_query($sql);
+
+	$carts = array();
+	$cate_counts = array();
+
+	$od_cart_count = 0;
+	for($i=0; $row=sql_fetch_array($result); $i++) {
+
+		$cate_counts[$row['ct_status']] += 1;
+
+		// 상품의 옵션정보
+		$sql = " select ct_id, mb_id, it_id, ct_price, ct_point, ct_qty, ct_stock_qty, ct_barcode, ct_option, ct_status, cp_price, ct_stock_use, ct_point_use, ct_send_cost, ct_sendcost, io_type, io_price, pt_msg1, pt_msg2, pt_msg3, ct_discount, ct_uid, ct_combine_ct_id
+						, ( SELECT prodSupYn FROM g5_shop_item WHERE it_id = MT.it_id ) AS prodSupYn
+						, prodMemo
+					from {$g5['g5_shop_cart_table']} MT
+					where od_id = '{$od_id}'
+						and it_id = '{$row['it_id']}'
+						and ct_uid = '{$row['ct_uid']}'
+					order by io_type asc, ct_id asc ";
+		$res = sql_query($sql);
+
+		$row['options_span'] = sql_num_rows($res);
+
+		$row['options'] = array();
+		for($k=0; $opt=sql_fetch_array($res); $k++) {
+
+			$opt_price = 0;
+
+			if($opt['io_type'])
+				$opt_price = $opt['io_price'];
+			else
+				$opt_price = $opt['ct_price'] + $opt['io_price'];
+
+			$opt["opt_price"] = $opt_price;
+			$od_cart_count += $opt["ct_qty"];
+
+			// 소계
+			$opt['ct_price_stotal'] = $opt_price * $opt['ct_qty'] - $opt['ct_discount'];
+			$opt['ct_point_stotal'] = $opt['ct_point'] * $opt['ct_qty'] - $opt['ct_discount'];
+
+			if($opt["prodSupYn"] == "Y"){
+				$opt["ct_price_stotal"] -= ($opt["ct_stock_qty"] * $opt_price);
+			}
+
+			$row['options'][] = $opt;
+		}
+
+
+		// 합계금액 계산
+		$sql = " select SUM(IF(io_type = 1, (io_price * ct_qty), ((ct_price + io_price) * (ct_qty - ct_stock_qty)))) as price,
+						SUM(ct_qty) as qty,
+						SUM(ct_discount) as discount,
+						SUM(ct_send_cost) as sendcost
+					from {$g5['g5_shop_cart_table']}
+					where it_id = '{$row['it_id']}'
+						and od_id = '{$od_id}'
+						and ct_uid = '{$row['ct_uid']}'";
+		$sum = sql_fetch($sql);
+
+		$row['sum'] = $sum;
+
+		$carts[] = $row;
+	}
+
+	return $carts;
+}
