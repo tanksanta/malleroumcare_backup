e6c9597558d0a0cca53ac96a0348c10ff74aa57d
liam <pelogvc@gmail.com>
2021-04-27 20:01:51 +0900

    feat: new index

--- www/main.php
+++ www/main.php
@@ -8,11 +8,11 @@ $is_index = true;
 $is_main = true;
 // 루트 index를 쇼핑몰 index 설정했을 때
 if(IS_YC && isset($default['de_root_index_use']) && $default['de_root_index_use'] && (!isset($ci) || !$ci)) {
-    require_once(G5_SHOP_PATH.'/main.php');
+    require_once(G5_SHOP_PATH.'/index.php');
     return;
 } else {
 	if(USE_G5_THEME && defined('G5_THEME_PATH')) {
-		require_once(G5_THEME_PATH.'/main.php');
+		require_once(G5_THEME_PATH.'/index.php');
 		return;
 	}
 	define('IS_SHOP', false);
@@ -31,7 +31,15 @@ include_once('./_head.php');
 if(!isset($config['as_thema']) || !$config['as_thema']) {
 	echo '<br><p align=center>아미나빌더가 설치되어 있지 않습니다. <br><br> 관리자 접속후 관리자화면 > 테마관리에서 아미나빌더를 설치해 주세요.</p><br>';
 } else {
-	include_once(THEMA_PATH.'/main.php');
+	if(IS_YC) {
+		if(file_exists(THEMA_PATH.'/index.php')) {
+			include_once(THEMA_PATH.'/index.php');
+		} else {
+			include_once(THEMA_PATH.'/shop.index.php');
+		}
+	} else {
+		include_once(THEMA_PATH.'/index.php');
+	}
 }
 
 include_once('./_tail.php');
