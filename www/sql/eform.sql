686519a98e698edf5d6fc226bb993572efc7b884
39hn <admin@the.gg>
2021-05-04 14:56:16 +0900

    전자계약서 DB 설계 중 - eform_document

---
+++ www/sql/eform.sql
@@ -0,0 +1,73 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eform_document`
+--
+
+DROP TABLE IF EXISTS `eform_document`;
+CREATE TABLE IF NOT EXISTS `eform_document` (
+  `dc_id` binary(16) NOT NULL, -- 문서 ID, PRI, UNHEX(REPLACE(UUID(),'-',''))
+  `dc_subject` varchar(255) NOT NULL DEFAULT '', -- 문서 제목
+  `dc_status` varchar(255) NOT NULL DEFAULT '', -- 서명 상태 (서명 대기, 서명 완료...) → todo: 생략 가능한가?
+  -- ---------
+  -- 사업소
+  -- ---------
+  -- 사업소 ID
+  `entId` varchar(255), -- ENT2020070900001
+  -- 사업소명
+  `entNm` varchar(255), -- 이로움사업소1
+  -- 사업자등록번호
+  `entCrn` varchar(255), -- 사업자번호 todo: system.eroumcare.com 이랑 연동할지 알아봐야함
+  -- 사업소이메일
+  `entMail` varchar(255),
+  -- 대표자명
+  `entCeoNm` varchar(255),
+  -- 특약사항1
+  `entConAcc01` longtext, -- 본 계약은 국민건강보험 노인장기요양보험 급여상품의 공급계약을 체결함에 목적이 있다.
+  -- 특약사항2
+  `entConAcc02` longtext, -- 본 계약서에 명시되지 아니한 사항이나 의견이 상이할 때에는 상호 협의하에 해결하는 것을 원칙으로 한다.
+  -- ---------
+  -- 수급자
+  -- ---------
+  -- 수급자 ID
+  `penId` varchar(255), -- PENID_20210111094719
+  -- 수급자 이름
+  `penNm` varchar(255), -- 테스트트
+  -- 수급자 전화번호
+  `penConNum` varchar(255), -- 010-8748-7796
+  -- 수급자 생년월일
+  `penBirth` varchar(255), -- 20210111
+  -- 장기요양인정번호
+  `penLtmNum` varchar(255), -- L123123123123
+  -- 장기요양등급
+  `penRecGraCd` varchar(255), -- 02
+  `penRecGraNm` varchar(255), -- 2등급
+  -- 본인부담금율
+  `penTypeCd` varchar(255), -- 02
+  `penTypeNm` varchar(255), -- 감경 6%
+  -- 이용기간
+  `penExpiDtm` varchar(255), -- 2021-01-11 ~ 2022-01-10
+  -- ---------
+  -- 계약서 생성 정보
+  -- ---------
+  -- 계약서 생성 시간
+  `dc_datetime` datetime NOT NULL default '0000-00-00 00:00:00',
+  -- 계약서 생성시 IP
+  `dc_ip` varchar(255) NOT NULL default '',
+  -- 계약서 생성 브라우저 정보
+  `dc_browser` longtext,
+  -- 직인 파일명
+  `dc_signUrl` varchar(255) NOT NULL default '',
+  -- ---------
+  -- 계약서 작성 정보
+  -- ---------
+  -- 계약서 작성 시간
+  `dc_sign_datetime` datetime NOT NULL default '0000-00-00 00:00:00',
+  -- 계약서 작성시 IP
+  `dc_sign_ip` varchar(255) NOT NULL default '',
+  -- 계약서 작성 브라우저 정보
+  `dc_sign_browser` longtext,
+  -- 사인 파일명
+  `dc_sign_signUrl` varchar(255) NOT NULL default '',
+  PRIMARY KEY (`dc_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
