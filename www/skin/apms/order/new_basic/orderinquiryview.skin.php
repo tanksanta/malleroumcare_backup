360c948297ec1c28875e21822d313f2cf5b40623
koreanleo <dhrn0623@naver.com>
2021-05-06 18:01:35 +0900

    사용자 배송비 표시

--- www/skin/apms/order/new_basic/orderinquiryview.skin.php
+++ www/skin/apms/order/new_basic/orderinquiryview.skin.php
@@ -611,14 +611,14 @@ if (document.referrer.indexOf("shop/orderform.php") >= 0) {
 						</div>
 					</li>
 					<?php } ?>
-
 					<?php if ($od['od_send_cost2'] > 0) { ?>
-						<div class="col-xs-6">추가배송비</div>
-						<div class="col-xs-6 text-right">
-							<strong><?php echo number_format($od['od_send_cost2']); ?> 원</strong>
-						</div>
+					<li>
+							<div class="col-xs-6">추가배송비</div>
+							<div class="col-xs-6 text-right">
+								<strong><?php echo number_format($od['od_send_cost2']); ?> 원</strong>
+							</div>
+					</li>
 					<?php } ?>
-
 
 					<li>
 						<div>
