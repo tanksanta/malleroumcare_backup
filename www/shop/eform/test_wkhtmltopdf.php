4575a644da216f47f5d7dd7837220acf6e3a603f
39hn <admin@the.gg>
2021-05-07 17:54:18 +0900

    wkhtmltopdf 테스트파일 이름변경

--- www/shop/eform/test_wkhtmltopdf.php
+++ www/shop/eform/test_wkhtmltopdf.php
@@ -1,6 +1,6 @@
 <?php
 $url = './thk001_2.html';
-exec('/usr/local/bin/wkhtmltopdf -L 0 -R 0 -T 0 -B 0 --enable-local-file-access --viewport-size 1240 "'.$url.'" "test.pdf"');
+exec('/usr/local/bin/wkhtmltopdf -L 0 -R 0 -T 0 -B 0 --enable-local-file-access --viewport-size 1240 "thk001_2.html" "test.pdf"');
 
 header("Content-type: application/pdf");
 header("Content-Disposition: inline; filename=test.pdf");
