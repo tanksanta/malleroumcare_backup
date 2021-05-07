ac1782a252f1e15872f860c8691f0a77c4cc458a
39hn <admin@the.gg>
2021-05-07 15:58:14 +0900

    fix: 복지용구공급계약서 2P 호환성

--- www/shop/eform/test.php
+++ www/shop/eform/test.php
@@ -1,5 +1,5 @@
 <?php
-$url = './thk001_1.html';
+$url = './thk001_2.html';
 exec('/usr/local/bin/wkhtmltopdf -L 0 -R 0 -T 0 -B 0 --enable-local-file-access --viewport-size 1240 "'.$url.'" "test.pdf"');
 
 header("Content-type: application/pdf");
