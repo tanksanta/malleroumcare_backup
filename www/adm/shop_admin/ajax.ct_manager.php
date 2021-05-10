7552f4660da7418dd831a9b02dae73d5dea7a87b
koreanleo <dhrn0623@naver.com>
2021-05-10 16:11:19 +0900

    출고담당자 ajax file

---
+++ www/adm/shop_admin/ajax.ct_manager.php
@@ -0,0 +1,20 @@
+<?php
+    include_once('./_common.php');
+    header('Content-Type: application/json');
+    if($_POST['ct_id']&&$_POST['ct_manager']){
+        $sql_ct = "UPDATE `g5_shop_cart` SET `ct_manager`='".$_POST['ct_manager']."' where `ct_id` = '".$_POST['ct_id']."'";
+        sql_query($sql_ct);
+        $ret = array(
+            'result' => 'success',
+        );
+        $json = json_encode($ret);
+        echo $json;
+    }else{
+        $ret = array(
+            'result' => 'fail',
+        );
+        $json = json_encode($ret);
+        echo $json;
+    }
+
+?>
