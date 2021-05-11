9c205cc889d62624fb26842b7152f375f3a81036
39hn <admin@the.gg>
2021-05-11 18:00:20 +0900

    계약서 생성 팝업 폼 퍼블리싱: fixed head & foot, text input

--- www/shop/eform/popup.writeEform.php
+++ www/shop/eform/popup.writeEform.php
@@ -15,11 +15,11 @@
   <script src="<?php echo G5_JS_URL ?>/jquery-1.11.3.min.js"></script>
   <style>
   * { margin: 0; padding: 0; box-sizing: border-box; position: relative; }
-  html, body { width: 100%; min-width: 100%; margin: 0 !important; padding: 0; font-family: "Noto Sans KR", sans-serif; font-size: 13px; }
+  html, body { width: 100%; min-width: 100%; height: 100%; min-height: 100%; margin: 0 !important; padding: 0; font-family: "Noto Sans KR", sans-serif; font-size: 13px; }
   button { display: inline-block; }
 
   #popupWrap {
-    padding-bottom: 12px;
+    height: 100%;
   }
 
   #popupWrap .flex {
@@ -34,12 +34,18 @@
     align-items: center;
   }
 
-  #popupWrap .head {
+  .popupHeadWrap {
+    position: fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height:60px;
     padding: 12px;
     border-bottom: 1px solid #ddd;
+    background-color: #fff;
   }
 
-  #popupWrap .head .title {
+  .popupHeadWrap .title {
     -webkit-flex: 1;          /* Chrome */
     -ms-flex: 1;              /* IE 10 */
     flex: 1;                  /* NEW, Spec - Opera 12.1, Firefox 20+ */
@@ -48,13 +54,23 @@
     padding: 0 6px;
   }
 
-  #popupWrap .head .menu {
+  .popupHeadWrap .menu {
+  }
+
+  .popupContentWrap {
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 100%;
+    bottom: 50px;
+    padding-bottom: 12px;
+    overflow-y: auto;
   }
 
   #btnResetEform {
     padding: 6px 12px;
     background-color: #f5f5f5;
-    border: 1px solid #dedede;
+    border: 1px solid #ddd;
     color: #666;
   }
 
@@ -161,9 +177,20 @@
     padding: 8px 6px;
   }
 
+  #prodRow input[type=text] {
+    width: 100%;
+    height: 24px;
+    border: 1px solid #ddd;
+  }
+
+  #prodRow input.period {
+    width: 50%;
+  }
+
   .btnDelProd {
     font-size: 24px;
-    height: 20px;
+    width: 24px;
+    height: 24px;
     line-height: 13px;
     vertical-align: middle;
     color: #666;
@@ -186,131 +213,187 @@
     display: block;
     width: 100%;
     height: 100px;
+    border: 1px solid #ddd;
     resize: vertical;
     padding: 8px;
+  }
+
+  .popupFootWrap {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+  }
+
+  .popupFootWrap button {
+    height: 100%;
+    font-weight: bold;
+    font-size: 20px;
+    color: #fff;
+  }
+
+  #btnSubmitEform {
+    -webkit-flex: 1;          /* Chrome */
+    -ms-flex: 1;              /* IE 10 */
+    flex: 1;                  /* NEW, Spec - Opera 12.1, Firefox 20+ */
+    background-color: #ee8102;
+  }
+
+  #btnCancelEform {
+    background-color: #787878;
+    width: 130px;
   }
   </style>
 </head>
 <body>
   <div id="popupWrap">
-    <div class="head flex">
+    <div class="popupHeadWrap flex">
       <h1 class="title">계약서 생성</h1>
       <div class="menu">
         <button id="btnResetEform">변경사항 초기화</button>
         <button id="btnCloseEform">&times;</button>
       </div>
     </div>
-    <div id="penRow" class="row">
-      <h3>수급자정보</h3>
-      <table id="tablePenInfo">
-        <tr>
-          <th>수급자</th>
-          <td>홍길동</td>
-        </tr>
-        <tr>
-          <th>장기요양인정번호</th>
-          <td>L11111121233</td>
-        </tr>
-        <tr>
-          <th>인정등급</th>
-          <td>2등급</td>
-        </tr>
-        <tr>
-          <th>구분</th>
-          <td>감경 6%</td>
-        </tr>
-      </table>
-    </div>
-    <div id="prodRow" class="row">
-      <div class="flex">
-        <h3>공급물품</h3>
-        <div class="right">
-          <span class="notice">*계약서 작성을 위해 추가하는 물품은 통합시스템에서 관리되지 않고 계약서 작성에만 활용됩니다.</span>
-          <label class="checkbox"><input id="chkConfirm" type="checkbox">확인함</label>
+    <div class="popupContentWrap">
+      <div id="penRow" class="row">
+        <h3>수급자정보</h3>
+        <table id="tablePenInfo">
+          <tr>
+            <th>수급자</th>
+            <td>홍길동</td>
+          </tr>
+          <tr>
+            <th>장기요양인정번호</th>
+            <td>L11111121233</td>
+          </tr>
+          <tr>
+            <th>인정등급</th>
+            <td>2등급</td>
+          </tr>
+          <tr>
+            <th>구분</th>
+            <td>감경 6%</td>
+          </tr>
+        </table>
+      </div>
+      <div id="prodRow" class="row">
+        <div class="flex">
+          <h3>공급물품</h3>
+          <div class="right">
+            <span class="notice">*계약서 작성을 위해 추가하는 물품은 통합시스템에서 관리되지 않고 계약서 작성에만 활용됩니다.</span>
+            <label class="checkbox"><input id="chkConfirm" type="checkbox">확인함</label>
+          </div>
+        </div>
+        <div class="prodContentWrap">
+          <div class="prodTableRow">
+            <div class="flex">
+              <div class="prodHead">구매물품</div>
+              <button id="btnAddBuyProd">추가</button>
+            </div>
+            <div class="prodTableWrap">
+              <table id="tableBuyProd">
+                <thead>
+                  <tr>
+                    <th>품목명</th>
+                    <th>제품명</th>
+                    <th>제품기호</th>
+                    <th>일련번호(바코드)</th>
+                    <th>개수</th>
+                    <th>판매계약일</th>
+                    <th>고시가</th>
+                    <th>본인부담금</th>
+                    <th>삭제</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Aaaa</td>
+                    <td>ddddd</td>
+                    <td>Fddddd</td>
+                    <td><input type="text" class="datePicker"></td>
+                    <td>2</td>
+                    <td>2021-02-02</td>
+                    <td>10원</td>
+                    <td>3원</td>
+                    <td><button class="btnDelProd">&times;</button></td>
+                  </tr>
+                  <tr>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" class="datePicker"></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><button class="btnDelProd">&times;</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="prodTableRow">
+            <div class="flex">
+              <div class="prodHead">대여물품</div>
+              <button id="btnAddRentProd">추가</button>
+            </div>
+            <div class="prodTableWrap">
+              <table id="tableBuyProd">
+                <thead>
+                  <tr>
+                    <th>품목명</th>
+                    <th>제품명</th>
+                    <th>제품기호</th>
+                    <th>일련번호(바코드)</th>
+                    <th>개수</th>
+                    <th>계약기간</th>
+                    <th>고시가</th>
+                    <th>본인부담금</th>
+                    <th>삭제</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Aaaa</td>
+                    <td>ddddd</td>
+                    <td>Fddddd</td>
+                    <td><input type="text" ></td>
+                    <td>2</td>
+                    <td>21-02-02 ~ 25-02-02</td>
+                    <td>10원</td>
+                    <td>3원</td>
+                    <td><button class="btnDelProd">&times;</button></td>
+                  </tr>
+                  <tr>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" class="datePicker period"><input type="text" class="datePicker period"></td>
+                    <td><input type="text" ></td>
+                    <td><input type="text" ></td>
+                    <td><button class="btnDelProd">&times;</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="prodContentWrap">
-        <div class="prodTableRow">
-          <div class="flex">
-            <div class="prodHead">구매물품</div>
-            <button id="btnAddBuyProd">추가</button>
-          </div>
-          <div class="prodTableWrap">
-            <table id="tableBuyProd">
-              <thead>
-                <tr>
-                  <th>품목명</th>
-                  <th>제품명</th>
-                  <th>제품기호</th>
-                  <th>일련번호(바코드)</th>
-                  <th>개수</th>
-                  <th>판매계약일</th>
-                  <th>고시가</th>
-                  <th>본인부담금</th>
-                  <th>삭제</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Aaaa</td>
-                  <td>ddddd</td>
-                  <td>Fddddd</td>
-                  <td></td>
-                  <td>2</td>
-                  <td>2021-02-02</td>
-                  <td>10원</td>
-                  <td>3원</td>
-                  <td><button class="btnDelProd">&times;</button></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div class="prodTableRow">
-          <div class="flex">
-            <div class="prodHead">대여물품</div>
-            <button id="btnAddRentProd">추가</button>
-          </div>
-          <div class="prodTableWrap">
-            <table id="tableBuyProd">
-              <thead>
-                <tr>
-                  <th>품목명</th>
-                  <th>제품명</th>
-                  <th>제품기호</th>
-                  <th>일련번호(바코드)</th>
-                  <th>개수</th>
-                  <th>계약기간</th>
-                  <th>고시가</th>
-                  <th>본인부담금</th>
-                  <th>삭제</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Aaaa</td>
-                  <td>ddddd</td>
-                  <td>Fddddd</td>
-                  <td></td>
-                  <td>2</td>
-                  <td>21-02-02 ~ 25-02-02</td>
-                  <td>10원</td>
-                  <td>3원</td>
-                  <td><button class="btnDelProd">&times;</button></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div id="entConAcc01Row" class="row entConAcc">
+        <h3>특약사항1</h3>
+        <textarea name="entConAcc01" id="entConAcc01">본 계약은 국민건강보험 노인장기요양보험 급여상품의 공급계약을 체결함에 목적이 있다.</textarea>
+      </div>
+      <div id="entConAcc02Row" class="row entConAcc">
+        <h3>특약사항2</h3>
+        <textarea name="entConAcc02" id="entConAcc02">본 계약서에 명시되지 아니한 사항이나 의견이 상이할 때에는 상호 협의하에 해결하는 것을 원칙으로 한다.</textarea>
       </div>
     </div>
-    <div id="entConAcc01Row" class="row entConAcc">
-      <h3>특약사항1</h3>
-      <textarea name="entConAcc01" id="entConAcc01">본 계약은 국민건강보험 노인장기요양보험 급여상품의 공급계약을 체결함에 목적이 있다.</textarea>
-    </div>
-    <div id="entConAcc02Row" class="row entConAcc">
-      <h3>특약사항2</h3>
-      <textarea name="entConAcc02" id="entConAcc02">본 계약서에 명시되지 아니한 사항이나 의견이 상이할 때에는 상호 협의하에 해결하는 것을 원칙으로 한다.</textarea>
+    <div class="popupFootWrap flex">
+      <button id="btnSubmitEform">계약서 생성</button>
+      <button id="btnCancelEform">취소</button>
     </div>
   </div>
 </body>
