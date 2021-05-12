ee7aa7b0dae5a5aa84065b66f940c5525a031162
39hn <admin@the.gg>
2021-05-12 19:01:14 +0900

    계약서 생성 전 조건 수정

--- www/skin/apms/order/new_basic/orderinquiryview.skin.php
+++ www/skin/apms/order/new_basic/orderinquiryview.skin.php
@@ -552,7 +552,7 @@ if (document.referrer.indexOf("shop/orderform.php") >= 0) {
 							<li class="eform-tab-head">공급계약서</li>
 							<li class="eform-tab-desc">수급자 주문시 간편하게 작성하는 온라인 계약</li>
 							<li class="eform-tab-links">
-								<?php if(!$eform["dc_id"]) { // 계약서 생성 전 ?>
+								<?php if(!$eform["dc_id"] || $eform["dc_status"] == '0') { // 계약서 생성 전 ?>
 								<a href="#" id="linkEformWrite" class="eform-tab-link">계약서 생성</a>
 								<?php } ?>
 							</li>
