d0945834fe5a8877da26c4984f195cfb5b3863d0
koreanleo <dhrn0623@naver.com>
2021-05-13 11:25:50 +0900

    creat statistics.php file

---
+++ www/adm/shop_admin/statistics.php
@@ -0,0 +1,32 @@
+<?php
+// $sub_menu = '500110';
+include_once('./_common.php');
+
+auth_check($auth[$sub_menu], "r");
+
+$g5['title'] = '타이틀';
+include_once (G5_ADMIN_PATH.'/admin.head.php');
+include_once(G5_PLUGIN_PATH.'/jquery-ui/datepicker.php');
+?>
+
+<!-- 퍼블섹션 -->
+<section>
+
+</section>
+
+<script>
+$(function() {
+    $("#date, #fr_date, #to_date").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: "yymmdd",
+        showButtonPanel: true,
+        yearRange: "c-99:c+99",
+        maxDate: "+0d"
+    });
+});
+</script>
+
+<?php
+include_once (G5_ADMIN_PATH.'/admin.tail.php');
+?>
