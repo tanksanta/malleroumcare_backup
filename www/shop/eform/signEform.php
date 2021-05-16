1f6630a4c5635c17813548c20de952947612c294
39hn <admin@the.gg>
2021-05-16 23:05:43 +0900

    계약서 작성 css 버그 수정

--- www/shop/eform/signEform.php
+++ www/shop/eform/signEform.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="css/default.css">
-  <link rel="stylesheet" href="css/signEform.css">
+  <link rel="stylesheet" href="css/signeform.css">
   <link rel="stylesheet" href="css/thk001.css">
   <link rel="stylesheet" href="css/thk002.css">
   <link rel="stylesheet" href="css/thk003.css">
