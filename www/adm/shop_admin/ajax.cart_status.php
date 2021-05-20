0088f96b9fe2311575e051a6bd3a64cfcdffa623
koreanleo <dhrn0623@naver.com>
2021-05-21 01:53:57 +0900

    상태 변경 로그 남기기 -cart 별

--- www/adm/shop_admin/ajax.cart_status.php
+++ www/adm/shop_admin/ajax.cart_status.php
@@ -8,21 +8,52 @@
         $add_sql="";
         $ct_ex_date = date("Y-m-d");
         $stateCd;
-        
-        for($i=0;$i<count($_POST['ct_id']); $i ++){
-            
-            if($_POST['step']=="완료"||$_POST['step']=="주문무효"||$_POST['step']=="취소"){
-                $sql_ct_s = "select a.it_id, a.mb_id, a.stoId, b.mb_entId from `g5_shop_cart` a left join `g5_member` b on a.mb_id = b.mb_id where `ct_id` = '".$_POST['ct_id'][$i]."'";
-                $result_ct_s=sql_fetch($sql_ct_s);
 
+        //상태값 치환
+        switch ($_POST['step']) {
+            case '보유재고등록': $ct_status_text="보유재고등록"; break;
+            case '재고소진': $ct_status_text="재고소진"; break;
+            case '작성': $ct_status_text="작성"; break;
+            case '주문무효': $ct_status_text="주문무효"; break;
+            case '취소': $ct_status_text="주문취소"; break;
+            case '주문': $ct_status_text="주문접수"; break;
+            case '입금': $ct_status_text="입금완료"; break;
+            case '준비': $ct_status_text="상품준비"; break;
+            case '출고준비': $ct_status_text="출고준비"; break;
+            case '배송': $ct_status_text="출고완료"; break;
+            case '완료': $ct_status_text="배송완료"; break;
+        }
+
+
+        for($i=0;$i<count($_POST['ct_id']); $i ++){
+            $sql_ct_s = "select a.od_id, a.it_id, a.it_name, a.ct_option, a.mb_id, a.stoId, b.mb_entId from `g5_shop_cart` a left join `g5_member` b on a.mb_id = b.mb_id where `ct_id` = '".$_POST['ct_id'][$i]."'";
+            $result_ct_s=sql_fetch($sql_ct_s);
+            $od_id=$result_ct_s['od_id'];
+            $content=$result_ct_s['it_name'];
+            if($result_ct_s['it_name'] !== $result_ct_s['ct_option']){
+                $content=$content."(".$result_ct_s['ct_option'].")";
+            }
+            $content =$content."-".$ct_status_text." 변경";
+            $sql = "INSERT INTO g5_shop_order_admin_log SET
+                od_id = '{$od_id}',
+                mb_id = '{$member['mb_id']}',
+                ol_content = '{$content}',
+                ol_datetime = now()
+            ";
+            //로그 insert
+            sql_query($sql);
+
+            //상태 update
+            if($_POST['step']=="배송"){$add_sql = ", `ct_ex_date` = '".$ct_ex_date."'"; }
+            $sql_ct = "update `g5_shop_cart` set `ct_status` = '".$_POST['step']."'".$add_sql." where `ct_id` = '".$_POST['ct_id'][$i]."'";
+            sql_query($sql_ct);
+
+            //시스템 상태값 변경
+            if($_POST['step']=="완료"||$_POST['step']=="주문무효"||$_POST['step']=="취소"){
                 $stoId=$stoId.$result_ct_s['stoId'];
                 $usrId=$result_ct_s['mb_id'];
                 $entId=$result_ct_s['mb_entId'];
             }
-            if($_POST['step']=="배송"){$add_sql = ", `ct_ex_date` = '".$ct_ex_date."'"; }
-            $sql_ct = "update `g5_shop_cart` set `ct_status` = '".$_POST['step']."'".$add_sql." where `ct_id` = '".$_POST['ct_id'][$i]."'";
-            sql_query($sql_ct);
-      
         }
 
         if($_POST['step']=="완료"||$_POST['step']=="주문무효"||$_POST['step']=="취소"){
