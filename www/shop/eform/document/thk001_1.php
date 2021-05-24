7787b5b27620bab7e11dbfb49ea8c4f19f00b58d
39hn <admin@the.gg>
2021-05-24 14:24:13 +0900

    계약서작성: THK001_1 데이터 연동&테이블 접근성

--- www/shop/eform/document/thk001_1.php
+++ www/shop/eform/document/thk001_1.php
@@ -14,33 +14,33 @@
         <col style="width: 30%" />
       </colgroup>
       <tr>
-        <th class="bold" rowspan="3">(갑)</th>
-        <th colspan="2">수급자</th>
-        <td colspan="3">기초1</td>
-        <th class="bold" rowspan="3">(을)</th>
-        <th>장기요양기관명</th>
-        <td>(주)다온테크</td>
+        <th scope="col" class="bold" rowspan="3">(갑)</th>
+        <th scope="col" colspan="2">수급자</th>
+        <td colspan="3"><?=$eform['penNm']?></td>
+        <th scope="col" class="bold" rowspan="3">(을)</th>
+        <th scope="col">장기요양기관명</th>
+        <td><?=$eform['entNm']?></td>
       </tr>
       <tr>
-        <th colspan="2">장기요양인정번호</th>
-        <td colspan="3">L1111111112</td>
-        <th>장기요양기관번호</th>
-        <td>126-88-01425</td>
+        <th scope="col" colspan="2">장기요양인정번호</th>
+        <td colspan="3"><?=$eform['penLtmNum']?></td>
+        <th scope="col">장기요양기관번호</th>
+        <td><?=$eform['entCrn']?></td>
       </tr>
       <tr>
-        <th colspan="2">인정등급</th>
-        <td>3등급</td>
-        <th>구분</th>
-        <td>기초 0%</td>
-        <th>대표자</th>
-        <td>최재호</td>
+        <th scope="col" colspan="2">인정등급</th>
+        <td><?=$eform['penRecGraNm']?></td>
+        <th scope="col">구분</th>
+        <td><?=$eform['penTypeNm']?></td>
+        <th scope="col">대표자</th>
+        <td><?=$eform['entCeoNm']?></td>
       </tr>
       <tr class="divider">
-        <th class="bold" colspan="2">대리인</th>
+        <th scope="col" class="bold" colspan="2">대리인</th>
         <td></td>
-        <th colspan="2">수급자와의 관계</th>
+        <th scope="col" colspan="2">수급자와의 관계</th>
         <td colspan="2"></td>
-        <th>전화번호</th>
+        <th scope="col">전화번호</th>
         <td></td>
       </tr>
     </table>
@@ -70,174 +70,66 @@
         <col style="width: 13%" />
         <col style="width: 13%" />
       </colgroup>
-      <tr>
-        <th rowspan="2">품목명</th>
-        <th rowspan="2">제품명</th>
-        <th colspan="2">제품코드</th>
-        <th rowspan="2">개수</th>
-        <th rowspan="2">판매계약일</th>
-        <th rowspan="2">급여가</th>
-        <th rowspan="2">본인부담금</th>
-      </tr>
-      <tr>
-        <th>품목코드</th>
-        <th>바코드</th>
-      </tr>
-      <tr>
-        <td>이동변기</td>
-        <td>APT-101</td>
-        <td>T03030060001</td>
-        <td>123456789012</td>
-        <td class="center">1</td>
-        <td>2021-04-07</td>
-        <td class="right">229,500</td>
-        <td class="right">0</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="center"></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <th class="bold" colspan="6">합계</th>
-        <td class="right">229,500</td>
-        <td class="right">0</td>
-      </tr>
+      <thead>
+        <tr>
+          <th scope="row" rowspan="2">품목명</th>
+          <th scope="row" rowspan="2">제품명</th>
+          <th scope="row" colspan="2">제품코드</th>
+          <th scope="row" rowspan="2">개수</th>
+          <th scope="row" rowspan="2">판매계약일</th>
+          <th scope="row" rowspan="2">급여가</th>
+          <th scope="row" rowspan="2">본인부담금</th>
+        </tr>
+        <tr>
+          <th scope="row">품목코드</th>
+          <th scope="row">바코드</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $total_buy_price = 0;
+        $total_buy_price_pen = 0;
+        $count = 0;
+        foreach($buy as $item) {
+        ?>
+        <tr>
+          <td><?=$item['ca_name']?></td>
+          <td><?=$item['it_name']?></td>
+          <td><?=$item['it_code']?></td>
+          <td><?=$item['it_barcode']?></td>
+          <td class="center"><?=$item['it_qty']?></td>
+          <td><?=$item['it_date']?></td>
+          <td class="right"><?=number_format($item['it_price'])?></td>
+          <td class="right"><?=number_format($item['it_price_pen'])?></td>
+        </tr>
+        <?php
+          $total_buy_price += intval($item['it_price']);
+          $total_buy_price_pen += intval($item['it_price_pen']);
+          $count++;
+        }
+
+        // 최소 15줄은 생성
+        for($i = $count; $i < 15; $i++) {
+        ?>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td class="center"></td>
+          <td></td>
+          <td class="right"></td>
+          <td class="right"></td>
+        </tr>
+        <?php
+        }
+        ?>
+        <tr>
+          <th scope="col" class="bold" colspan="6">합계</th>
+          <td class="right"><?=number_format($total_buy_price)?></td>
+          <td class="right"><?=number_format($total_buy_price_pen)?></td>
+        </tr>
+      </tbody>
     </table>
     <h3 class="thk001_h3">대여 품목</h3>
     <table class="thk001_table item_table">
@@ -250,68 +142,63 @@
         <col style="width: 13%" />
         <col style="width: 13%" />
       </colgroup>
-      <tr>
-        <th rowspan="2">품목명</th>
-        <th rowspan="2">제품명</th>
-        <th colspan="2">제품코드</th>
-        <th rowspan="2">계약기간</th>
-        <th rowspan="2">급여가</th>
-        <th rowspan="2">본인부담금</th>
-      </tr>
-      <tr>
-        <th>품목코드</th>
-        <th>바코드</th>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class="right"></td>
-        <td class="right"></td>
-      </tr>
-      <tr>
-        <th class="bold" colspan="5">합계</th>
-        <td class="right">0</td>
-        <td class="right">0</td>
-      </tr>
+      <thead>
+        <tr>
+          <th scope="row" rowspan="2">품목명</th>
+          <th scope="row" rowspan="2">제품명</th>
+          <th scope="row" colspan="2">제품코드</th>
+          <th scope="row" rowspan="2">계약기간</th>
+          <th scope="row" rowspan="2">급여가</th>
+          <th scope="row" rowspan="2">본인부담금</th>
+        </tr>
+        <tr>
+          <th scope="row">품목코드</th>
+          <th scope="row">바코드</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          $total_rent_price = 0;
+          $total_rent_price_pen = 0;
+          $count = 0;
+          foreach($rent as $item) {
+        ?>
+        <tr>
+          <td><?=$item['ca_name']?></td>
+          <td><?=$item['it_name']?></td>
+          <td><?=$item['it_code']?></td>
+          <td><?=$item['it_barcode']?></td>
+          <td><?=$item['it_date']?></td>
+          <td class="right"><?=number_format($item['it_price'])?></td>
+          <td class="right"><?=number_format($item['it_price_pen'])?></td>
+        </tr>
+        <?php
+          $total_rent_price += intval($item['it_price']);
+          $total_rent_price_pen += intval($item['it_price_pen']);
+          $count++;
+        }
+
+        // 최소 5줄은 생성
+        for($i = $count; $i < 5; $i++) {
+        ?>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td class="right"></td>
+            <td class="right"></td>
+          </tr>
+        <?php
+        }
+        ?>
+        <tr>
+          <th scole="col" class="bold" colspan="5">합계</th>
+          <td class="right"><?=number_format($total_rent_price)?></td>
+          <td class="right"><?=number_format($total_rent_price_pen)?></td>
+        </tr>
+      </tbody>
     </table>
     <table class="thk001_table item_table">
       <colgroup>
@@ -320,9 +207,9 @@
         <col style="width: 13%" />
       </colgroup>
       <tr>
-        <th class="bold">구매/대여 합계</th>
-        <td class="right">229,500</td>
-        <td class="right">0</td>
+        <th scope="col" class="bold">구매/대여 합계</th>
+        <td class="right"><?=number_format($total_buy_price + $total_rent_price)?></td>
+        <td class="right"><?=number_format($total_buy_price_pen + $total_rent_price_pen)?></td>
       </tr>
     </table>
   </div>
