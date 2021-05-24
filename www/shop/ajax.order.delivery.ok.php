7f3ed84f72f42bc94084d704466eaa2c33ad6ddd
koreanleo <dhrn0623@naver.com>
2021-05-25 00:16:32 +0900

    1_6 배송완료 버튼 완료

--- www/shop/ajax.order.delivery.ok.php
+++ www/shop/ajax.order.delivery.ok.php
@@ -9,7 +9,6 @@ $sql = " select ct.*, od.stoId as sto_id
               and ct.od_id = '$od_id'
               and ct.mb_id = '{$member['mb_id']}'";
 $result = sql_fetch($sql); // and ct.ct_status = '배송' 
-
 if (!$result['ct_id']) {
     $ret = array(
         'result' => 'fail',
@@ -18,55 +17,19 @@ if (!$result['ct_id']) {
     die(json_encode($ret));
 }
 
-// 이로움 STOID 가져오기
-$api_data = array(
-    'usrId' => $member['mb_id'],
-    'entId' => $member['mb_entId'],
-    'prodId' => $result['it_id'],
+$sql = "
+update {$g5['g5_shop_cart_table']}
+    set ct_status = '완료' 
+    where ct_id = '{$result['ct_id']}'
+";
+sql_query($sql);
+
+$ret = array(
+    'result' => 'success',
+    'msg' => '배송완료로 수정되었습니다.',
 );
-$api_result = get_eroumcare(EROUMCARE_API_SELECT_DETAIL_LIST, $api_data);
+die(json_encode($ret));
 
-$sto_ids = explode(',', $result['sto_id']);
 
-if ($api_result['data'] && count($api_result['data'])) {
-    $filtered_arr = array_values(array_filter($api_result['data'], function($arr) {
-        global $sto_ids;
-        return in_array($arr['stoId'], $sto_ids);
-    }));
-}
 
-$new_sto_ids = array_map(function($data) {
-    return array(
-        'stoId' => $data['stoId'],
-        'stateCd' => '01',
-    );
-}, $filtered_arr);
 
-// 배송완료 재고있음 상태 변경
-$api_data = array(
-    'usrId' => $member['mb_id'],
-    'entId' => $member['mb_entId'],
-    'prods' => $new_sto_ids,
-);
-
-$api_result = get_eroumcare(EROUMCARE_API_STOCK_UPDATE, $api_data);
-if ($api_result['errorYN'] === 'N') {
-    
-    $sql = "
-    update {$g5['g5_shop_cart_table']}
-        set ct_status = '배송완료' 
-        where ct_id = '{$result['ct_id']}'
-    ";
-    sql_query($sql);
-
-    $ret = array(
-        'result' => 'success',
-        'msg' => '배송완료로 수정되었습니다.',
-    );
-    die(json_encode($ret));
-}
-
-die(json_encode(array(
-    'result' => 'fail',
-    'msg' => '알수없는 오류로 실패하였습니다.',
-)));
