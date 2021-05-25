d4ad5a642ad37650c6b2164bf13535eb7cf5df02
39hn <admin@the.gg>
2021-05-25 19:25:14 +0900

    계약서작성: 서명 폼 입력

--- www/shop/eform/signEform.php
+++ www/shop/eform/signEform.php
@@ -35,6 +35,7 @@ while($item = sql_fetch_array($items)) {
   <link rel="stylesheet" href="css/thk001.css">
   <link rel="stylesheet" href="css/thk002.css">
   <link rel="stylesheet" href="css/thk003.css">
+  <script src="<?=G5_JS_URL?>/signature_pad.umd.js"></script>
   <script src="<?=G5_JS_URL?>/jquery-1.11.3.min.js"></script>
 </head>
 <body>
@@ -57,12 +58,200 @@ while($item = sql_fetch_array($items)) {
       <button id="btnNext">다음단계</button>
     </div>
   </div>
+  <div id="popup-sign">
+    <div class="popup-modal-wrap">
+      <div class="popup-modal">
+        <div class="head-wrap">서명하기</div>
+        <div id="sign-pad">
+          <canvas></canvas>
+          <div id="sign-back">이곳에 사인해주세요.</div>
+        </div>
+        <div class="bottom-wrap">
+          <button id="btn-sign-submit">확인</button><button id="btn-sign-cancel">취소</button>
+        </div>
+      </div>
+    </div>
+  </div>
   <script>
   $(function() {
     $('#btnCloseSign').click(function(e) {
       e.preventDefault();
       window.close();
     });
+
+    function closeSignPopup() {
+      $('body').removeClass('modal-open');
+      var $popUp = $('#popup-sign');
+      resizeHandler();
+      $popUp.data('id', '');
+      $popUp.css({display: 'none'});
+    }
+
+    $('#btn-sign-cancel').click(function(e) {
+      e.preventDefault();
+
+      closeSignPopup();
+    });
+
+    $('#btn-sign-submit').click(function(e) {
+      e.preventDefault();
+      var id = $('#popup-sign').data('id');
+
+      if(id) {
+        if (signaturePad.isEmpty()) {
+          return alert("서명을 입력해주세요.");
+        } else {
+          var dataURL = toResizedDataURL(canvas, origWidth, origHeight);
+          state[id] = dataURL;
+          repaint();
+        }
+      }
+
+      closeSignPopup();
+    });
+
+    $(document).on('click', '.btn-sign', function(e) {
+      e.preventDefault();
+      var id = $(this).data('id');
+
+      $('body').addClass('modal-open');
+      var $popUp = $('#popup-sign');
+      $popUp.data('id', id);
+      $popUp.css({display: 'table'});
+      origWidth = pos[id].width;
+      origHeight = pos[id].height;
+      resizeHandler();
+    });
+
+    var origWidth = 120;
+    var origHeight = 40;
+
+    var wrapper = document.getElementById('sign-pad');
+    var canvas = wrapper.querySelector('canvas');
+
+    var signaturePad = new SignaturePad(canvas, {
+      backgroundColor: 'transparent',
+      minDistance: 5,
+      throttle: 3,
+      minWidth: 4,
+      maxWidth: 4
+    });
+
+    function calcSize(size, a, b) {
+      var ratio = a / b;
+      return size / ratio;
+    }
+
+    function resizeModal() {
+      var margin = 20;
+
+      var windowWidth = $(window).width();
+      var windowHeight = $(window).height();
+      var modalWidth = 700 + margin;
+      var modalHeight = 600 + margin;
+
+      if(windowWidth > modalWidth) {
+        if(windowHeight > modalHeight) {
+          modalWidth = modalWidth;
+          modalHeight = modalHeight;
+        } else {
+          modalWidth = calcSize(modalWidth, modalHeight, windowHeight);
+          modalHeight = windowHeight;
+        }
+      } else {
+        if(windowHeight > modalHeight) {
+          modalHeight = calcSize(modalHeight, modalWidth, windowWidth);
+          modalWidth = windowWidth;
+        } else {
+          if(windowWidth < windowHeight) {
+            modalHeight = calcSize(modalHeight, modalWidth, windowWidth);
+            modalWidth = windowWidth;
+          } else {
+            modalWidth = calcSize(modalWidth, modalHeight, windowHeight);
+            modalHeight = modalHeight;
+          }
+        }
+      }
+
+      modalWidth -= margin;
+      modalHeight -= margin;
+
+      $('.popup-modal').css({
+        width: modalWidth,
+        height: modalHeight
+      });
+
+      $('#sign-pad canvas').css({
+        top: 70,
+        left: 0,
+        width: modalWidth,
+        height: modalHeight - 70 - 55
+      });
+    }
+
+    function resizeCanvas() {
+      var ratio = Math.max(window.devicePixelRatio || 1, 1);
+
+      canvas.width = canvas.offsetWidth * ratio;
+      canvas.height = canvas.offsetHeight * ratio;
+      canvas.getContext('2d').scale(ratio, ratio);
+
+      signaturePad.clear();
+    }
+
+    function resizeSignBack(origWidth, origHeight) {
+      var margin = 20;
+      var canvasWidth = $('#sign-pad canvas').width() - margin;
+      var canvasHegiht = $('#sign-pad canvas').height();
+      var dpiRatio = origWidth / canvasWidth;
+      var newHeight = origHeight / dpiRatio;
+
+      $('#sign-back').css({
+        top: (canvasHegiht / 2) - (newHeight / 2) + 70,
+        left: margin / 2,
+        width: canvasWidth,
+        height: newHeight
+      });
+
+      /*$('#sign-pad canvas').css({
+        top: (canvasHegiht / 2) - (newHeight / 2) + 70,
+        left: margin / 2,
+        width: canvasWidth,
+        height: newHeight
+      });*/
+
+      signaturePad.minWidth = 0.75 / dpiRatio;
+      signaturePad.maxWidth = 0.75 / dpiRatio;
+    }
+
+    function resizeHandler() {
+      resizeModal();
+      resizeSignBack(origWidth, origHeight);
+      resizeCanvas();
+    }
+
+    function toResizedDataURL(canvas, origWidth, origHeight) {
+      var canvasWidth = canvas.width;
+      var canvasHeight = canvas.height;
+
+      var dpiRatio = origWidth / canvasWidth;
+
+      var resizedCanvas = document.createElement('canvas');
+      var resizedContext = resizedCanvas.getContext('2d');
+
+      resizedCanvas.width = origWidth * 5;
+      resizedCanvas.height = origHeight * 5;
+
+      var $signBack = $('#sign-back');
+
+      resizedContext.drawImage(canvas,
+        $signBack.css('left').replace(/[^-\d\.]/g, ''), $signBack.css('top').replace(/[^-\d\.]/g, '') - 70,
+        $signBack.width(), $signBack.height(),
+        0, 0,
+        origWidth * 5, origHeight * 5
+      );
+      return resizedCanvas.toDataURL();
+    }
 
     var pos = {
       sign_001_1: {
@@ -143,11 +332,16 @@ while($item = sql_fetch_array($items)) {
         // 서명이 비어있다면
         if(!state[id]) {
           $wrap.html('<button class="btn-sign" data-id="'+id+'">서명하기</button>');
+        } else {
+          var imageURL = state[id];
+          $wrap.html('<img src="'+imageURL+'" height="'+pos[id].height+'" alt="수급자 서명">');
         }
       });
     }
 
     repaint();
+
+    window.onresize = resizeHandler;
   });
   </script>
 </body>
