27a42b9f24da5902199c0d752bbe9a9530310b39
39hn <admin@the.gg>
2021-05-26 19:21:30 +0900

    재가서비스 이용내역서 퍼블리싱

---
+++ www/shop/eform/document/thk004.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../css/signeform.css">
+  <link rel="stylesheet" href="../css/thk004.css">
+  <style>
+    .a4 { width: 1200px; margin: 0 auto; }
+  </style>
+</head>
+<body>
+<div id="thk004" class="a4">
+  <div class="thk004">
+    <table class="thk004-table">
+      <colgroup>
+        <col style="width: 5%" />
+        <col style="width: 7%" />
+        <col style="width: 8%" />
+        <col style="width: 10%" />
+        <col style="width: 5%" />
+        <col style="width: 5%" />
+        <col style="width: 10%" />
+        <col style="width: 10%" />
+        <col style="width: 10%" />
+        <col style="width: 10%" />
+        <col style="width: 5%" />
+        <col style="width: 15%" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th scope="row" colspan="12" class="thk004-h1">재가서비스 이용내역서</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="col" rowspan="2">수급자</th>
+          <th scope="col" colspan="2">성명</th>
+          <td colspan="4" class="center">홍길동</td>
+          <th scope="col" colspan="2">주민등록번호</th>
+          <td colspan="3" class="center">null</td>
+        </tr>
+        <tr>
+          <th scope="col" colspan="2">장기요양등급</th>
+          <td colspan="4" class="center">3등급</td>
+          <th scope="col" colspan="2">장기요양인정번호</th>
+          <td colspan="3" class="center">L111111112</td>
+        </tr>
+        <tr>
+          <th scope="col" rowspan="6"><p>급여</p><p>이용</p><p>신청</p><p>내역</p></th>
+          <th scope="col" colspan="2">급여종류</th>
+          <td colspan="4" class="center">재가급여</td>
+          <th scope="col" colspan="2">이용기간</th>
+          <td colspan="3" class="center">2020-01-02~2022-12-01</td>
+        </tr>
+        <tr>
+          <th scope="row" rowspan="2" colspan="2">서비스<br>종류</th>
+          <th scope="row" rowspan="2" colspan="2">서비스내용</th>
+          <th scope="row" rowspan="2" colspan="2">수가</th>
+          <th scope="row" rowspan="2">횟수 / 월</th>
+          <th scope="row" rowspan="2">금액 / 월</th>
+          <th scope="row" colspan="3">이용희망기관</th>
+        </tr>
+        <tr>
+          <th scope="row" colspan="2">장기요양<br>기관명</th>
+          <th scope="row">장기요양<br>기관기호</th>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <th scope="col" colspan="4">합 &nbsp;계</th>
+          <td colspan="4" class="right">( 원 )</td>
+          <td colspan="3" class="slash">&nbsp;</td>
+        </tr>
+        <tr>
+          <th scope="col" rowspan="18"><p>복지</p><p>용구</p><p>이용</p><p>신청</p><p>내역</p></th>
+          <th scope="row" rowspan="2" colspan="2">품목명</th>
+          <th scope="row" rowspan="2">제품코드</th>
+          <th scope="row" colspan="2">급여방식</th>
+          <th scope="row" rowspan="2" colspan="2">대여기간</th>
+          <th scope="row" rowspan="2">금 &nbsp;액</th>
+          <th scope="row" colspan="3">이용희망기관</th>
+        </tr>
+        <tr>
+          <th scope="row">구입</th>
+          <th scope="row">대여</th>
+          <th scope="row" colspan="2">장기요양<br>기관명</th>
+          <th scope="row">장기요양<br>기관기호</th>
+        </tr>
+        <tr>
+          <td colspan="2">이동변기</td>
+          <td>T03030060001</td>
+          <td class="center">V(1)</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">229,500</td>
+          <td colspan="2">(주)다온테크</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td class="center">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td class="right">&nbsp;</td>
+          <td colspan="2">&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <th scope="col" colspan="3">합 &nbsp;계</th>
+          <td colspan="5" class="right">229,500 ( 원 )</td>
+          <td colspan="3" class="slash">&nbsp;</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+</body>
+</html>
