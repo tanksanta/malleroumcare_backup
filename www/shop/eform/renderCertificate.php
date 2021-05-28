8d35ca255879b281314438b287a6798d836b25c0
39hn <admin@the.gg>
2021-05-28 15:32:29 +0900

    감사추적인증서 퍼블리싱

---
+++ www/shop/eform/renderCertificate.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>감사추적 인증서</title>
+  <link rel="stylesheet" href="css/default.css">
+  <link rel="stylesheet" href="css/certificate.css">
+</head>
+<body>
+<div class="a4">
+  <div class="head">감사 추적 인증서 (제작시간 : 2021-04-28 17:31:09)</div>
+  <div class="body">
+    <div class="section">
+      <h3>감사 추적 인증서</h3>
+      <table>
+        <colgroup>
+          <col style="width: 20%;">
+          <col style="width: 80%;">
+        </colgroup>
+        <tr>
+          <th>문서 이름</th>
+          <td>이로움사업소1_1234567891_김기초L11111_20210527_001</td>
+        </tr>
+        <tr>
+          <th>문서 ID</th>
+          <td>e8675c90a7fb11ebaf0079d1d10cccb7</td>
+        </tr>
+        <tr>
+          <th>서명 상태</th>
+          <td>완료 됨</td>
+        </tr>
+        <tr>
+          <th>기준 시간</th>
+          <td>(UTC+09:00) 한국 표준시</td>
+        </tr>
+        <tr>
+          <th>문서 페이지 수</th>
+          <td>6P</td>
+        </tr>
+      </table>
+    </div>
+    <div class="section">
+      <h3>서명 정보</h3>
+      <table class="table-sign-info">
+        <colgroup>
+          <col style="width: 20%;">
+          <col style="">
+          <col style="width: 300px;">
+        </colgroup>
+        <thead>
+          <tr>
+            <th colspan="2">사업소</th>
+            <th>서명</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>문서</th>
+            <td>복지용구공급계약서</td>
+            <td class="img" rowspan="4">서명이미지</td>
+          </tr>
+          <tr>
+            <th>이름</th>
+            <td>사업소명</td>
+          </tr>
+          <tr>
+            <th>이메일</th>
+            <td>aaaa@naver.com</td>
+          </tr>
+          <tr>
+            <th>진행 정보</th>
+            <td>서명 완료</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table-sign-info">
+        <colgroup>
+          <col style="width: 20%;">
+          <col style="">
+          <col style="width: 300px;">
+        </colgroup>
+        <thead>
+          <tr>
+            <th colspan="2">수급자</th>
+            <th>서명</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>문서</th>
+            <td>복지용구공급계약서</td>
+            <td class="img" rowspan="4">서명이미지</td>
+          </tr>
+          <tr>
+            <th>이름</th>
+            <td>수급자명</td>
+          </tr>
+          <tr>
+            <th>이메일</th>
+            <td>aaaa@naver.com</td>
+          </tr>
+          <tr>
+            <th>진행 정보</th>
+            <td>서명 완료</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="section">
+      <h3>진행 이력</h3>
+      <table class="table-history">
+        <colgroup>
+          <col style="width: 20%;">
+          <col style="width: 80%;">
+        </colgroup>
+        <thead>
+          <tr>
+            <th>진행 시점 및 환경</th>
+            <th>진행 내용</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>
+              <div class="time">2021-04-28 17:29:54</div>
+              <div class="ip">221.162.130.10</div>
+            </th>
+            <td>전자계약서가 생성되었습니다.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="section">
+      <h3>전자서명 이용약관</h3>
+    </div>
+  </div>
+</div>
+</body>
+</html>
