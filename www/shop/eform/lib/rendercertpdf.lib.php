e32a230589eb6d6db7b6ffa7f7c9adefcb872387
39hn <admin@the.gg>
2021-05-31 16:26:15 +0900

    감사추적인증서 테스트 코드 삭제

--- www/shop/eform/lib/rendercertpdf.lib.php
+++ www/shop/eform/lib/rendercertpdf.lib.php
@@ -28,8 +28,6 @@ foreach($options as $key => $val) {
   if($val !== null) $args .= ' '.$val;
 }
 
-echo $args;
-
 exec("wkhtmltopdf{$args} \"{$G5_URL}/shop/eform/renderCertificate.php?od_id={$eform['od_id']}&uuid={$uuid}&entId={$eform['entId']}&penId={$eform['penId']}\" \"{$certdir}\"");
 
 ?>
