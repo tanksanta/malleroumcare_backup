3563c1574999ba6740cf2dbb253195b5328ca30d
39hn <admin@the.gg>
2021-06-01 12:19:05 +0900

    css 캐시 초기화

--- www/thema/eroumcare/assets/thema.head.php
+++ www/thema/eroumcare/assets/thema.head.php
@@ -3,7 +3,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 <link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/font.css"/>
 <link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/slick.css"/>
-<link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/common_new.css?v=2103191512"/>
+<link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/common_new.css?v=2106011218"/>
 <script src="<?php echo THEMA_URL ?>/assets/js/jquery.cookie.js"></script>
 <script src="<?php echo THEMA_URL ?>/assets/js/slick.min.js"></script>
 <script src="<?php echo THEMA_URL ?>/assets/js/common_new.js"></script>
