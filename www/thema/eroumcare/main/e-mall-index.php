d04303d1b974c2e3eabeb1b35af2008ce121952a
39hn <admin@the.gg>
2021-06-05 11:46:22 +0900

    메인화면: 디자인 적용 (슬라이드 미적용)

--- www/thema/eroumcare/main/e-mall-index.php
+++ www/thema/eroumcare/main/e-mall-index.php
@@ -5,84 +5,34 @@
 ?>
 
 <div class="main_top_wrap">
-	<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_top_desc.png" alt="" />
-	<p>
-		<a href="<?php echo G5_URL; ?>/main.php">전체 상품 보기</a>
-	</p>
-	
+	<div class="main_slider_nav">
+		<div class="active">복지용구 관리</div>
+		<!--<div>방문급여</div>
+		<div>시설급여</div>
+		<div>주야간보호급여</div>
+		<div>재무회계</div>-->
+	</div>
+	<div class="main_slider">
+		<div class="main_slide">
+			<div class="main_slide_head">
+				<div class="desc">복지용구 사업소 업무를 쉽고 간편하게</div>
+				<h2>복지용구 통합관리 시스템</h2>
+			</div>
+			<img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_service_info_01.png" alt="복지용구 통합관리 시스템">
+		</div>
+	</div>
 </div>
+
 <div class="service_info">
 	<div class="service_tit">
-		이로움 주요서비스
-		<p>장기요양기관을 위한 편안하게 관리할 수 있는 <span class="m_br"></span> 통합관리시스템을 제공합니다.</p>
+		서비스 이용문의
+		<p>복지용구 판매 사업소 및 요양센터 운영담당자 들의 문의를 받습니다.</p>
 	</div>
-	<ul>
-		<li>
-			<div class="service_desc">
-				<p>SYSYEM <span>01</span></p>
-				<p>복지용구급여</p>
-				<p>복지용구 사업소<br>업무편의를 쉽고 간편하게</p>
-				<a href="<?php echo G5_URL; ?>/main.php">바로가기</a>
-			</div>
-			<div class="service_icon">
-				<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_service_icon_01.png" alt="" />
-			</div>
-		</li>
-		<li>
-			<div class="service_desc">
-				<p>SYSYEM <span>02</span></p>
-				<p>시설급여</p>
-				<p>노인요양시설 / 노인요양<br>공동생활가정의 업무를<br>쉽고 간편하게</p>
-			</div>
-			<div class="service_icon">
-				<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_service_icon_02.png" alt="" />
-			</div>
-		</li>
-		<li>
-			<div class="service_desc">
-				<p>SYSYEM <span>03</span></p>
-				<p>주야간보호급여</p>
-				<p>주야간 보호급여센터<br>업무를 쉽고 간편하게</p>
-			</div>
-			<div class="service_icon">
-				<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_service_icon_03.png" alt="" />
-			</div>
-		</li>
-		<li>
-			<div class="service_desc">
-				<p>SYSYEM <span>04</span></p>
-				<p>방문급여</p>
-				<p>방문요양/간호/목욕 시설의<br>업무를 쉽고 간편하게</p>
-			</div>
-			<div class="service_icon">
-				<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_service_icon_04.png" alt="" />
-			</div>
-		</li>
-		<li>
-			<div class="service_desc">
-				<p>SYSYEM <span>05</span></p>
-				<p>재무회계</p>
-				<p>W4C 연동 재무회계서비스를<br>쉽고 간편하게</p>
-			</div>
-			<div class="service_icon">
-				<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_service_icon_05.png" alt="" />
-			</div>
-		</li>
-		<li>
-			<div class="service_desc">
-				<p>SYSYEM <span>06</span></p>
-				<p>시니어타운</p>
-				<p>노후를 즐겁게하는 실버 커뮤니티<br>각종정보/재취업/<br>노인용품/실버프로그램</p>
-			</div>
-			<div class="service_icon">
-				<img src="<?php echo G5_URL; ?>/thema/eroumcare/assets/img/main_service_icon_06.png" alt="" />
-			</div>
-		</li>
-	</ul>
+	<a href="#" onclick="alert('준비 중입니다.')" class="service_link">간편 문의하기</a>
 </div>
 
 <!-- 메인 배너 -->
-	<div id="mainBannerWrap">
+	<!--<div id="mainBannerWrap">
 		<div class="listWrap">
 			<div class="slick">
 				<div class="item">
@@ -93,7 +43,7 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</div>-->
 	
 
 <!-- 메인 최근게시글 -->
