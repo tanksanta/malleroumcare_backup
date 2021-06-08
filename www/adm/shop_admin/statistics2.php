bd85f7cf37922ab127e258fc95a9bb1fb210b94a
koreanleo <dhrn0623@naver.com>
2021-06-08 11:03:50 +0900

    작업 페이지 추가

---
+++ www/adm/shop_admin/statistics2.php
@@ -0,0 +1,18 @@
+<?php
+$sub_menu = '500010';
+include_once('./_common.php');
+// auth_check($auth[$sub_menu], "r");
+
+$g5['title'] = '운영관리통계';
+include_once (G5_ADMIN_PATH.'/admin.head.php');
+include_once(G5_PLUGIN_PATH.'/jquery-ui/datepicker.php');
+?>
+
+
+<section>
+
+</section>
+
+<?php
+include_once (G5_ADMIN_PATH.'/admin.tail.php');
+?>
