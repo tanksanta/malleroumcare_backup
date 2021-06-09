fdf41cc9ac849d1f375e676464b215ed6fdbf48f
39hn <admin@the.gg>
2021-06-09 17:27:47 +0900

    메인: 카테고리 클릭 먹통 수정

--- www/thema/eroumcare/assets/thema.head.php
+++ www/thema/eroumcare/assets/thema.head.php
@@ -6,5 +6,5 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 <link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/common_new.css?v=2106070221"/>
 <script src="<?php echo THEMA_URL ?>/assets/js/jquery.cookie.js"></script>
 <script src="<?php echo THEMA_URL ?>/assets/js/slick.min.js"></script>
-<script src="<?php echo THEMA_URL ?>/assets/js/common_new.js?v=2106021127"></script>
+<script src="<?php echo THEMA_URL ?>/assets/js/common_new.js?v=2106091723"></script>
 <script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
