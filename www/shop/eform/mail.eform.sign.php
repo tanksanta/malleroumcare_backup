83f1f1113e52b94f19f08af956b16fa970fe002a
39hn <admin@the.gg>
2021-06-10 16:46:22 +0900

    메일: 계약서 서명시 메일 퍼블리싱

---
+++ www/shop/eform/mail.eform.sign.php
@@ -0,0 +1,109 @@
+<?php
+//if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>계약서 체결 완료 메일</title>
+</head>
+<body>
+  <div style="background:#f7f7f7;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0">
+    <div style="-webkit-text-size-adjust:none;background:#fff;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0 auto;max-width:595px;padding:0;width:100%">
+      <table border="0" cellspacing="0" cellpadding="0" style="font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0;width:100%">
+        <tbody>
+          <tr>
+            <td style="border-top:4px solid #f07b01;border-bottom:1px solid #ced3d6;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:27px 21px 24px;text-align:center">
+              <table border="0" cellpadding="0" cellspacing="0" style="font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0;width:100%">
+                <tbody>
+                  <tr>
+                    <td style="width:120px;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:24px;margin:0;padding:0">
+                      <img src="http://mall.eroumcare.doto.li/thema/eroumcare/assets/img/top_logo.jpg" width="120" alt="이로움" style="border:0;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0" loading="lazy">
+                    </td>
+                    <td style="text-align:right;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:24px;margin:0;padding:0">
+                      안전하고 편리한 이로움 전자문서시스템
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+          
+          <tr>
+            <td style="font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:22px 21px 37px;text-align:center">
+              <table border="0" cellpadding="0" cellspacing="0" style="font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0;width:100%">
+                <tbody>
+                  <tr>
+                    <td height="15" style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:0"></td>
+                  </tr>
+                  <tr>
+                    <td style="font-weight:bold;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:18px;letter-spacing:-1px;line-height:23px;margin:0;padding:0">
+                      전자문서가 도착했습니다.
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="15" style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:0"></td>
+                  </tr>
+                  <tr>
+                    <td style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:18px;letter-spacing:-1px;line-height:23px;margin:0;padding:0">
+                      이로움 사이트를 통해서 확인이 가능합니다.
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="15" style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:0"></td>
+                  </tr>
+                  <tr>
+                    <td style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:0 20px;">
+                      <table border="0" cellpadding="0" cellspacing="0" style="font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0;width:100%">
+                        <tbody>
+                          <tr>
+                            <td style="border-top:1px solid #ced3d6;text-align:left;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:20px 0">문서 이름</td>
+                            <td style="border-top:1px solid #ced3d6;text-align:left;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:20px 0">사업소명_1112233333_수급자명L11123_20210102_001</td>
+                          </tr>
+                          <tr>
+                            <td style="border-top:1px solid #ced3d6;width:150px;text-align:left;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:10px 0">문서 내용</td>
+                            <td style="border-top:1px solid #ced3d6;text-align:left;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:10px 0">1. 개인정보이용수집동의서</td>
+                          </tr>
+                          <tr>
+                            <td>&nbsp;</td>
+                            <td style="text-align:left;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:10px 0">2. 복지용구공급계약서</td>
+                          </tr>
+                          <tr>
+                            <td style="border-bottom:1px solid #ced3d6;">&nbsp;</td>
+                            <td style="border-bottom:1px solid #ced3d6; text-align:left;color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:10px 0">3. 장기요양급여제공기록지</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td height="23" style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:0"></td>
+                  </tr>
+                  <tr>
+                    <td style="color:#696969;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:0">
+                      <a href="#" style="color:#ffffff;background-color:#f07b01;display:inline-block;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;font-size:15px;letter-spacing:-1px;line-height:23px;margin:0;padding:10px 40px;text-decoration:none;">전자문서 확인</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+          
+          <tr>
+            <td style="background:#f8f8f9;font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:27px 21px 69px">
+              <table border="0" cellpadding="0" cellspacing="0" style="font-family:AppleSDGothicNeo-Regular,'나눔고딕',NanumGothic,'맑은고딕',Malgun Gothic,dotum,'돋움',Dotum,Helvetica;margin:0;padding:0;width:100%">
+                <tbody>
+                  <tr>
+                    <td style="color:#767676;font-family:Helvetica;font-size:12px;line-height:17px;margin:0;padding:0">Copyright ⓒ <strong>Eroumcare</strong>. All Rights Reserved.</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
+</html>
