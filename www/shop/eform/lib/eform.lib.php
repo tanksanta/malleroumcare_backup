b57bd0feec596ab368a1ca7986fcb71ac67e5640
39hn <admin@the.gg>
2021-06-17 23:07:02 +0900

    상품관리: 상품 업데이트시 백엔드에서 시스템 api호출하게 변경

--- www/shop/eform/lib/eform.lib.php
+++ www/shop/eform/lib/eform.lib.php
@@ -53,17 +53,16 @@ function api_post_call($url, $data = null, $port = 9901) {
   return api_call($url, 'POST', $data, $port);
 }
 
-function api_firebase_call($url, $data) {
+function post_formdata($url, $data = null, $port = 9901) {
   $ch=curl_init();
+
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_PORT, $port);
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_POST, true);
-  if($data) curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  if($data) curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
-  
   curl_setopt($ch, CURLOPT_VERBOSE, true);
-  
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   
