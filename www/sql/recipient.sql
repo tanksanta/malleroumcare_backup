6c42602157392dba73e3f993e84d22582008bf88
39hn <admin@the.gg>
2021-06-22 08:56:27 +0900

    수급자연결관리: 수급자-사업소 관계 테이블

--- www/sql/recipient.sql
+++ www/sql/recipient.sql
@@ -10,3 +10,15 @@ CREATE TABLE IF NOT EXISTS `recipient_memo` (
   PRIMARY KEY (`me_id`),
   KEY `penId` (`penId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- 수급자연결관리-수급자사업소 relationship 테이블
+DROP TABLE IF EXISTS `recipient_link_rel`;
+CREATE TABLE IF NOT EXISTS `recipient_link_rel` (
+  `rl_id` int(11) NOT NULL,
+  `mb_no` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL default '',
+  `created_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `updated_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY (`rl_id`, `mb_no`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
