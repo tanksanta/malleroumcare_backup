902942c5a8067c4817fd49abe76409efa903edeb
liam <pelogvc@gmail.com>
2021-06-27 09:33:32 +0900

    feat: 출고담당자 지정시 push

--- www/adm/shop_admin/ajax.ct_manager.php
+++ www/adm/shop_admin/ajax.ct_manager.php
@@ -9,6 +9,25 @@
             $ct_manager = $_POST['ct_manager'];
         }
 
+        $sql = "SELECT * FROM `g5_shop_cart` WHERE ct_id = '{$ct_id}'";
+        $cart = sql_fetch($sql);
+
+        $ct_it_name = $cart['it_name'];
+        $ct_option = ($cart["ct_option"] == $cart['it_name']) ? "" : "(".$cart['ct_option'].")";
+        $ct_it_name = $ct_it_name.$ct_option;
+
+        // 주문자 정보
+        $order_member = get_member($cart['mb_id']);
+        $giup_name = $order_member['mb_giup_bname'] ? "[" . $order_member['mb_giup_bname'] . "] " : "";
+
+        add_notification(
+            array(),
+            $ct_manager,
+            '신규 출고 담당자로 지정된 상품이 있습니다.',
+            $giup_name . $ct_it_name . " * " . $cart['ct_qty'] . "개",
+            // 'http://naver.com',
+        );
+
         $sql_ct = "UPDATE `g5_shop_cart` SET `ct_manager`='".$ct_manager."' where `ct_id` = '".$_POST['ct_id']."'";
         sql_query($sql_ct);
         $ret = array(
