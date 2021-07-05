493749c52bae11c3ada747b6c8607860e7dca088
39hn <admin@the.gg>
2021-07-05 12:32:03 +0900

    청구관리: 공단파일 수급자구분/본인부담금으로 수급자 본인부담율 구하는 함수 추가

--- www/shop/claim_manage_test.php
+++ www/shop/claim_manage_test.php
@@ -1,6 +1,38 @@
 <?php
 include_once('./_common.php');
 
+function parse_pen_type($penType, $penRate) {
+  $penTypeCd = '';
+
+  switch($penRate) {
+    case 14:
+    case 15:
+    case 16:
+      if($penType === '일반')
+        $penTypeCd = '00'; // 일반 15%
+      break;
+    case 8:
+    case 9:
+    case 10:
+      if($penType === '일반')
+        $penTypeCd = '01'; // 감경 9%
+      break;
+    case 5:
+    case 6:
+    case 7:
+      if($penType === '일반')
+        $penTypeCd = '02'; // 감경 6%
+      else if($penType === '의료')
+        $penTypeCd = '03'; // 의료 6%
+      break;
+    case 0:
+      if($penType === '의료')
+        $penTypeCd = '04'; // 기초 0%
+  }
+
+  return $penTypeCd;
+}
+
 if($file = $_FILES['file']['tmp_name']) {
   include_once('../lib/Excel/reader.php');
 
@@ -22,16 +54,18 @@ if($file = $_FILES['file']['tmp_name']) {
       $row = $sheet['cells'][$r];
       
       $pen = array(
-        'penType' => $row[3],
-        'penNm' => $row[4],
-        'penLtmNum' => $row[$i == 0 ? 7 : 5],
-        'penRecGraNm' => $row[$i == 0 ? 9 : 6],
+        'penType' => trim($row[3]),
+        'penNm' => trim($row[4]),
+        'penLtmNum' => trim($row[$i == 0 ? 7 : 5]),
+        'penRecGraNm' => trim($row[$i == 0 ? 9 : 6]),
         'start_date' => date('Y-m-d', strtotime($row[$i == 0 ? 10 : 7])),
         'total_price' => intval(preg_replace("/[^0-9]/", "", $row[$i == 0 ? 12 : 8])),
         'total_price_pen' => intval(preg_replace("/[^0-9]/", "", $row[$i == 0 ? 13 : 9])),
         'total_price_ent' => intval(preg_replace("/[^0-9]/", "", $row[$i == 0 ? 16 : 10]))
       );
       $pen['penRate'] = intval($pen['total_price_pen'] == 0 ? 0 : round($pen['total_price_pen'] * 100 / $pen['total_price']));
+      $pen['penTypeCd'] = parse_pen_type($pen['penType'], $pen['penRate']);
+      $pen['penTypeNm'] = $pen_type_cd[$pen['penTypeCd']];
       $pen_arr[] = $pen;
     }
   }
