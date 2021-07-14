eb1bfbcc5fdf3491e479cf0ce3d7cc6532280226
39hn <admin@the.gg>
2021-07-14 11:30:27 +0900

    9_5. 빈 페이지 추가

---
+++ www/shop/disinfection_ledger_pay_list.php
@@ -0,0 +1,17 @@
+<?php
+include_once('./_common.php');
+
+if(!$is_member){
+  alert("접근 권한이 없습니다.");
+  exit;
+}
+
+$g5['title'] = "수금관리";
+include_once("./_head.php");
+?>
+
+<div>내용</div>
+
+<?php
+include_once('./_tail.php');
+?>
