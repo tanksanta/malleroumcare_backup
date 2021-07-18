421d6d1778c74747a2db5d82ca7a0ea965f69625
liam <pelogvc@gmail.com>
2021-07-18 16:52:20 +0900

    수급자 리스트 상세 주문내역 페이지 연결

--- www/shop/my_recipient_view.php
+++ www/shop/my_recipient_view.php
@@ -166,7 +166,7 @@ $recs = get_recs_by_recipient($pen['penId']);
     <div class="main_btn_wrap">
       <a href="<?=G5_SHOP_URL.'/electronic_manage.php?penId='.$pen['penId']?>" class="primary">전자문서 확인</a>
       <a href="<?=G5_SHOP_URL.'/claim_manage.php?penId='.$pen['penId']?>" class="secondary">청구관리</a>
-      <a href="<?=G5_SHOP_URL.'/connect_recipient.php?pen_id='.$pen['penId'].'&redirect='.urlencode('/shop/cart.php')?>">주문내역</a>
+      <a href="<?=G5_SHOP_URL.'/orderinquiry.php?sel_field=all&search='.$pen['penId']?>">주문내역</a>
     </div>
   </div>
 
