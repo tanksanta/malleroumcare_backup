a5a00fda5dc22f22c930caa3401765706fa0206f
39hn <admin@the.gg>
2021-07-19 17:13:08 +0900

    거래처원장: DB 구조 수정

--- www/sql/ledger.sql
+++ www/sql/ledger.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS `ledger_content`;
 CREATE TABLE IF NOT EXISTS `ledger_content` (
   `lc_id` int(11) NOT NULL auto_increment, -- 원장 id (PRI, AI)
   `mb_id` varchar(255) NOT NULL default '',
-  `lc_type` tinyint(1) NOT NULL COMMENT '0: 입금 / 1: 출금',
+  `lc_type` tinyint(1) NOT NULL COMMENT '1: 입금 / 2: 출금',
   `lc_amount` int(11) NOT NULL DEFAULT 0,
   `lc_memo` varchar(255) NOT NULL,
   `lc_created_at` datetime NOT NULL default '0000-00-00 00:00:00',
-  `lc_updated_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `lc_created_by` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`lc_id`),
   KEY `mb_id` (`mb_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
