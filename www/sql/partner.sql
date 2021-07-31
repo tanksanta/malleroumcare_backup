539019f5c0bfebe079ec1cbf766db1cc4a8d6972
39hn <admin@the.gg>
2021-07-31 18:02:35 +0900

    파트너주문내역: DB 테이블 수정

--- www/sql/partner.sql
+++ www/sql/partner.sql
@@ -1,24 +1,23 @@
 -- 파트너 설치결과 보고서 테이블
 DROP TABLE IF EXISTS `partner_install_report`;
 CREATE TABLE IF NOT EXISTS `partner_install_report` (
-  `ir_id` int(11) NOT NULL auto_increment,
   `od_id` bigint(20) NOT NULL default 0,
   `mb_id` varchar(30) NOT NULL default '',
   `ir_issue` text NOT NULL,
-  `ir_report_url` varchar(255) NOT NULL default '',
+  `ir_cert_url` varchar(255) NOT NULL default '',
   `ir_created_at` datetime NOT NULL default '0000-00-00 00:00:00',
   `ir_updated_at` datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY (`ir_id`),
-  UNIQUE KEY `od_id_mb_id` (`od_id`, `mb_id`)
+  PRIMARY KEY (`od_id`, `mb_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- 파트너 설치결과 보고서 설치사진 업로드 테이블
 DROP TABLE IF EXISTS `partner_install_photo`;
 CREATE TABLE IF NOT EXISTS `partner_install_photo` (
   `ip_id` int(11) NOT NULL auto_increment,
-  `ir_id` int(11) NOT NULL,
+  `od_id` bigint(20) NOT NULL default 0,
+  `mb_id` varchar(30) NOT NULL default '',
   `ip_photo_url` varchar(255) NOT NULL default '',
   `ip_created_at` datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY (`ip_id`),
-  KEY `ir_id` (`ir_id`)
+  KEY `od_id_mb_id` (`od_id`, `mb_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
