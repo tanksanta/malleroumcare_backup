c0d54c95780f7f1f42cc82232c23634a34aa647f
liam <pelogvc@gmail.com>
2021-08-02 10:28:18 +0900

    회원타입 일반회원만 튜토리얼 보이도록 수정

--- www/thema/eroumcare/shop.head.php
+++ www/thema/eroumcare/shop.head.php
@@ -614,7 +614,7 @@ scrollToTop();
 		</div>
 		<?php
 		$tutorials = get_tutorials();
-		if (($member['mb_id'] && $tutorials && $tutorials['completed_count'] < 4) || $open_tutorial_popup) {
+		if (($member['mb_id'] && $member['mb_type'] === 'default' && $tutorials && $tutorials['completed_count'] < 4) || $open_tutorial_popup) {
 			$tutorial_percent = round($tutorials['completed_count'] / 4 * 100);
 			$tutorial_percent = $tutorial_percent < 5 ? 5 : $tutorial_percent;
 		?>
