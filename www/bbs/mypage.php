ed8877ddd205b7c2032fb91e44e8d1039143ffb6
39hn <admin@the.gg>
2021-08-03 16:15:40 +0900

    마이페이지: 쿠폰갯수 안보이던 오류 수정

--- www/bbs/mypage.php
+++ www/bbs/mypage.php
@@ -5,6 +5,20 @@ define('_MYPAGE_', true);
 if (!$is_member)
     goto_url(G5_BBS_URL."/login.php?url=".urlencode(G5_BBS_URL."/mypage.php"));
 
+// 쿠폰
+$cp_count = 0;
+$sql = " select cp_id
+            from {$g5['g5_shop_coupon_table']}
+            where mb_id IN ( '{$member['mb_id']}', '전체회원' )
+              and cp_start <= '".G5_TIME_YMD."'
+              and cp_end >= '".G5_TIME_YMD."' ";
+$res = sql_query($sql);
+
+for($k=0; $cp=sql_fetch_array($res); $k++) {
+  if(!is_used_coupon($member['mb_id'], $cp['cp_id']))
+  $cp_count++;
+}
+
 $mb_homepage = set_http(clean_xss_tags($member['mb_homepage']));
 $mb_profile = ($member['mb_profile']) ? conv_content($member['mb_profile'],0) : '';
 $mb_signature = ($member['mb_signature']) ? apms_content(conv_content($member['mb_signature'], 1)) : '';
