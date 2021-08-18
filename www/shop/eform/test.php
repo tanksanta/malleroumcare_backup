c0859d3387d0a13317801b42a9e8117530985879
39hn <admin@the.gg>
2021-08-18 11:04:45 +0900

    알림톡 테스트 코드 수정

--- www/shop/eform/test.php
+++ www/shop/eform/test.php
@@ -4,7 +4,7 @@ include_once("./_common.php");
 if($is_admin != 'super') alert('접근 ㄴㄴ');
 
 if($hp)
-  $result = send_alim_talk('test', $hp, 'ent_eform_result', '홍길동님과 전자계약이 체결되었습니다.');
+  $result = send_alim_talk('test', $hp, 'ent_eform_result', "[이로움]\n\n홍길동님과 전자계약이 체결되었습니다.");
 else
   $result = get_biztalk_result();
 var_dump($result);
