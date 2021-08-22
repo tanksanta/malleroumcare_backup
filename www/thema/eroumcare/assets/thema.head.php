c4647e15a39a38ca42619ecc273e583ca3372fbb
39hn <admin@the.gg>
2021-08-22 16:08:24 +0900

    css, js 캐시 초기화

--- www/thema/eroumcare/assets/thema.head.php
+++ www/thema/eroumcare/assets/thema.head.php
@@ -3,8 +3,8 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 <link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/font.css?v=2106021127"/>
 <link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/slick.css"/>
-<link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/common_new.css?v=2108111730"/>
+<link type="text/css" rel="stylesheet" href="<?php echo THEMA_URL ?>/assets/css/common_new.css?v=2108221607"/>
 <script src="<?php echo THEMA_URL ?>/assets/js/jquery.cookie.js"></script>
 <script src="<?php echo THEMA_URL ?>/assets/js/slick.min.js"></script>
-<script src="<?php echo THEMA_URL ?>/assets/js/common_new.js?v=2106091723"></script>
+<script src="<?php echo THEMA_URL ?>/assets/js/common_new.js?v=2107221607"></script>
 <script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
