6ad7783f045245757ed14cacd0af5d06eed67937
39hn <admin@the.gg>
2021-08-26 17:06:30 +0900

    대여종료 오류 수정

--- www/shop/ajax.stock.update.php
+++ www/shop/ajax.stock.update.php
@@ -1,33 +1,35 @@
 <?php
-  $prods = $_POST['prods'];
-  if(is_array($prods)) {
-    foreach($prods as $prod) {
-      if($prod['stateCd'] == '09') { // 대여종료
-        sql_query("
-          UPDATE
-            stock_custom_order
-          SET
-            sc_rent_state = 'done',
-            sc_updated_at = NOW()
-          WHERE
-            sc_stoId = '{$prod['stoId']}' and
-            sc_rent_state = 'rent'
-        ");
-      }
+include_once('./_common.php');
+
+$prods = $_POST['prods'];
+if(is_array($prods)) {
+  foreach($prods as $prod) {
+    if($prod['stateCd'] == '09') { // 대여종료
+      sql_query("
+        UPDATE
+          stock_custom_order
+        SET
+          sc_rent_state = 'done',
+          sc_updated_at = NOW()
+        WHERE
+          sc_stoId = '{$prod['stoId']}' and
+          sc_rent_state = 'rent'
+      ");
     }
   }
-	// header("Content-Type: application/json");
+}
+// header("Content-Type: application/json");
 
-	$oCurl = curl_init();
-	curl_setopt($oCurl, CURLOPT_PORT, 9901);
-	curl_setopt($oCurl, CURLOPT_URL, "https://system.eroumcare.com/api/stock/update");
-	curl_setopt($oCurl, CURLOPT_POST, 1);
-	curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($oCurl, CURLOPT_POSTFIELDS, json_encode($_POST, JSON_UNESCAPED_UNICODE));
-	curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($oCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-	$res = curl_exec($oCurl);
-	curl_close($oCurl);
-    echo $res;
+$oCurl = curl_init();
+curl_setopt($oCurl, CURLOPT_PORT, 9901);
+curl_setopt($oCurl, CURLOPT_URL, "https://system.eroumcare.com/api/stock/update");
+curl_setopt($oCurl, CURLOPT_POST, 1);
+curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($oCurl, CURLOPT_POSTFIELDS, json_encode($_POST, JSON_UNESCAPED_UNICODE));
+curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($oCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+$res = curl_exec($oCurl);
+curl_close($oCurl);
+  echo $res;
 
 ?>
