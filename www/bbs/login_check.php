9e5166bf59da825ab16f207e4346aac075ff76bc
39hn <admin@the.gg>
2021-09-01 10:27:59 +0900

    로그인: 시스템 비밀번호와 쇼핑몰 비밀번호가 다를 시 쇼핑몰 비밀번호를 시스템 비밀번호와 동기화 (임시조치)

--- www/bbs/login_check.php
+++ www/bbs/login_check.php
@@ -66,13 +66,23 @@ if($_POST["mb_id"] != "admin" && $check_member['mb_type'] !== 'normal') {
     }
 
   } else if($res["errorYN"] == "N") {
-    /*//계정정보 불러오기
+    //계정정보 불러오기
     $mb = get_member($mb_id);
+
+    if(!$mb['mb_id'])
+      alert('존재하지 않는 계정입니다.');
 
     //쇼핑몰에 등록이 되어 있지 않으면, 메세지출력
     if (!check_password($mb_password, $mb['mb_password'])) {
-      alert('가입된 회원아이디가 아니거나 비밀번호가 틀립니다.\\n비밀번호는 대소문자를 구분합니다.');
-    }*/
+      // 시스템 비밀번호는 맞는데 쇼핑몰 비밀번호가 틀린경우
+      // 임시로 쇼핑몰 비밀번호를 시스템 비밀번호와 동기화시킴
+      sql_query("
+        UPDATE {$g5["member_table"]}
+        SET mb_password = '".get_encrypt_string($mb_password)."'
+        WHERE mb_id = '$mb_id'
+      ");
+      //alert('가입된 회원아이디가 아니거나 비밀번호가 틀립니다.\\n비밀번호는 대소문자를 구분합니다.');
+    }
   } else {
     alert("시스템 서버가 응답하지 않습니다.");
   }
--- www/skin/member/basic/register_form.skin.php
+++ www/skin/member/basic/register_form.skin.php
@@ -1,4 +1,7 @@
 <?php
+if($member['mb_id'] && !$w) {
+  goto_url('/bbs/member_confirm.php?url=register_form.php');
+}
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
