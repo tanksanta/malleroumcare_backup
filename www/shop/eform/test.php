e9155368b370dd1a8083198626aea6e6fb716613
39hn <admin@the.gg>
2021-10-12 16:56:18 +0900

    상품사이즈 일괄업로드 테스트 코드

--- www/shop/eform/test.php
+++ www/shop/eform/test.php
@@ -3,9 +3,31 @@ include_once("./_common.php");
 
 if($is_admin != 'super') alert('접근 ㄴㄴ');
 
-if($hp)
-  $result = send_alim_talk('test', $hp, 'ent_eform_result', "[이로움]\n\n홍길동님과 전자계약이 체결되었습니다.");
-else
-  $result = get_biztalk_result();
-var_dump($result);
+include_once(G5_LIB_PATH."/PHPExcel.php");
+$reader = PHPExcel_IOFactory::createReader('Excel5');
+$excel = $reader->load('./boxsize.xls');
+$sheet = $excel->getActiveSheet();
+
+$num_rows = $sheet->getHighestRow();
+
+for($i = 2; $i <= $num_rows; $i++) {
+  $it_id = sql_real_escape_string(trim($sheet->getCell('C'.$i)->getValue()));
+  $it_box_size_width = sql_real_escape_string(trim($sheet->getCell('AH'.$i)->getValue()));
+  $it_box_size_length = sql_real_escape_string(trim($sheet->getCell('AI'.$i)->getValue()));
+  $it_box_size_height = sql_real_escape_string(trim($sheet->getCell('AJ'.$i)->getValue()));
+
+  if(!$it_id) continue;
+
+  // 박스 규격
+  $it_box_size = [
+    $it_box_size_width,
+    $it_box_size_length,
+    $it_box_size_height
+  ];
+  $it_box_size = implode(chr(30), $it_box_size);
+
+  sql_query(" UPDATE g5_shop_item SET it_box_size = '$it_box_size' WHERE it_id = '$it_id' ");
+}
+
+echo 'OK';
 ?>
