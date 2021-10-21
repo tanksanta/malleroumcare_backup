0e2664ab81d5e173a1ad428a993b69dc6e266006
39hn <admin@the.gg>
2021-10-21 10:38:08 +0900

    품목메세지: 요약정보, 탭

--- www/shop/item_msg_render.php
+++ www/shop/item_msg_render.php
@@ -1,19 +1,49 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 
+$list = [];
+$total = 0;
 foreach($items as $item) {
   $it = sql_fetch(" SELECT * FROM g5_shop_item WHERE it_id = '{$item['it_id']}' ");
   $gubun = $cate_gubun_table[substr($it["ca_id"], 0, 2)];
-  $gubun_text = '판매';
-  if($gubun == '01') $gubun_text = '대여';
-  else if($gubun == '02') $gubun_text = '비급여';
+  $gubun = '판매';
+  if($gubun == '01') $gubun = '대여';
+  else if($gubun == '02') $gubun = '비급여';
+
+  $it['gubun'] = $gubun;
+
+  $list[] = $it;
+  $total += $it['it_cust_price'];
+}
 ?>
+<div id="sum" class="im_tab_wr im_flex">
+  <a href="#sum" class="im_tab active">추천상품 요약</a>
+  <a href="#info" class="im_tab">상품상세정보</a>
+</div>
+<?php foreach($list as $it) { ?>
+<div class="im_sum_row im_flex">
+  <div class="im_sum_name"><?="{$it['it_name']} ({$it['gubun']})"?></div>
+  <div class="im_sum_price"><?=number_format($it['it_cust_price'])?>원</div>
+</div>
+<?php } ?>
+<div class="im_sum_total">
+  <p class="total_price"><?=number_format($total)?>원</p>
+  <p class="personal_price">
+    ※ 본인부담금 <span>15%(<?=number_format($total * 0.15)?>원)</span>, <span>9%(<?=number_format($total * 0.09)?>원)</span>, <span>6%(<?=number_format($total * 0.06)?>원)</span>
+  </p>
+</div>
+
+<div id="info" class="im_tab_wr im_flex">
+  <a href="#sum" class="im_tab">추천상품 요약</a>
+  <a href="#info" class="im_tab active">상품상세정보</a>
+</div>
+<?php foreach($list as $it) { ?>
 <div class="im_item">
   <div class="im_flex">
     <img src="/data/item/<?=$it['it_img1']?>" alt="<?=$it['it_name']?>" onerror="this.src='/img/no_img.png';">
     <div class="im_info">
       <p class="it_name">
-        <?php echo "{$it['it_name']} ({$gubun_text})"; ?>
+        <?php echo "{$it['it_name']} ({$it['gubun']})"; ?>
       </p>
       <p class="it_price">
         급여가 : <?php echo number_format($it["it_cust_price"]); ?>원
@@ -39,9 +69,8 @@ foreach($items as $item) {
   </div>
   <button class="im_btn_more">상세정보 펼쳐보기 ▼</button>
 </div>
-<?php
-}
-?>
+<?php } ?>
+
 <script>
 $('.im_btn_more').click(function() {
   $(this).closest('.im_item').addClass('active');
