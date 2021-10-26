df8432a1cd70f4261094db260cd1bd4d0d5c623e
39hn <admin@the.gg>
2021-10-26 15:07:24 +0900

    관리자 주문내역: 주문번호 클릭시에만 주문상세 이동

--- www/adm/shop_admin/pop.stock.add.php
+++ www/adm/shop_admin/pop.stock.add.php
@@ -1,5 +1,5 @@
 <?php
-// $sub_menu = '400400';
+// $sub_menu = '400620';
 include_once('./_common.php');
 include_once(G5_ADMIN_PATH.'/apms_admin/apms.admin.lib.php');
 
--- www/adm/shop_admin/pop.stock.add_result.php
+++ www/adm/shop_admin/pop.stock.add_result.php
@@ -1,5 +1,5 @@
 <?php
-$sub_menu = '400400';
+$sub_menu = '400620';
 include_once('./_common.php');
 
 auth_check($auth[$sub_menu], "w");
