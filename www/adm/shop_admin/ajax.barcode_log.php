09bb4c54e6c4eb0e0f311b30c6e62ce6a70eac61
39hn <admin@the.gg>
2021-11-08 17:26:17 +0900

    3_2. 바코드값 변경된 경우만 바코드로그 기록

--- www/adm/shop_admin/ajax.barcode_log.php
+++ www/adm/shop_admin/ajax.barcode_log.php
@@ -2,14 +2,25 @@
 include_once('./_common.php');
 $data=date("Y-m-d H:i:s");
 
-for($i=0; $i<count($_POST['prods']); $i++){
+for($i=0; $i<count($_POST['prods']); $i++) {
     $sql_it = "select * from g5_shop_item where it_id ='".$_POST['prods'][$i]['prodId']."'";
     $result_it = sql_fetch($sql_it);
     $option = "";
     if($_POST['prods'][$i]['prodColor']) $option .= "(".$_POST['prods'][$i]['prodColor'].")";
     if($_POST['prods'][$i]['prodSize']) $option .= "(".$_POST['prods'][$i]['prodSize'].")";
-    $content = "바코드입력 : ".$result_it['it_name'].$option."[ ".$_POST['prods'][$i]['prodBarNum']." ]";
-    sql_query('INSERT INTO `g5_barcode_log`(`od_id`, `mb_id`, `stoId`, `barcode`, `b_content`, `b_date`) VALUES ("'.$_POST['od_id'].'","'.$_POST['mb_id'].'","'.$_POST['prods'][$i]['stoId'].'","'.$_POST['prods'][$i]['prodBarNum'].'","'.$content.'","'.$data.'")');
+
+    $sql = " SELECT barcode FROM g5_barcode_log WHERE od_id = '{$_POST['od_id']}' and stoId = '{$_POST['prods'][$i]['stoId']}' ORDER BY b_num DESC LIMIT 1 ";
+    $last_barcode = sql_fetch($sql);
+
+    if($last_barcode['barcode'] != $_POST['prods'][$i]['prodBarNum']) {
+        if($_POST['prods'][$i]['prodBarNum']) {
+            $content = "바코드입력 : ".$result_it['it_name'].$option."[ ".$_POST['prods'][$i]['prodBarNum']." ]";
+            sql_query('INSERT INTO `g5_barcode_log`(`od_id`, `mb_id`, `stoId`, `barcode`, `b_content`, `b_date`) VALUES ("'.$_POST['od_id'].'","'.$_POST['mb_id'].'","'.$_POST['prods'][$i]['stoId'].'","'.$_POST['prods'][$i]['prodBarNum'].'","'.$content.'","'.$data.'")');
+        } else {
+            $content = "바코드제거 : ".$result_it['it_name'].$option."[ ".$last_barcode['barcode']." ]";
+            sql_query('INSERT INTO `g5_barcode_log`(`od_id`, `mb_id`, `stoId`, `barcode`, `b_content`, `b_date`) VALUES ("'.$_POST['od_id'].'","'.$_POST['mb_id'].'","'.$_POST['prods'][$i]['stoId'].'","'.$_POST['prods'][$i]['prodBarNum'].'","'.$content.'","'.$data.'")');
+        }
+    }
 }
 
 
