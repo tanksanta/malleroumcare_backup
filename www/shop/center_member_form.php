865817df13eb2fc95ee9c7a36686a3bd6cd3aeb3
39hn <admin@the.gg>
2021-11-21 11:56:40 +0900

    직원등록: 정보수정

--- www/shop/center_member_form.php
+++ www/shop/center_member_form.php
@@ -4,6 +4,17 @@ include_once('./_common.php');
 $g5['title'] = "직원관리";
 include_once("./_head.php");
 
+if($w == 'u') {
+    $cm_code = clean_xss_tags($_POST['cm_code']);
+    $cm = sql_fetch("
+        SELECT * FROM center_member
+        WHERE mb_id = '{$member['mb_id']}' and cm_code = '$cm_code'
+    ");
+
+    if(!$cm['cm_id'])
+        alert('해당 직원이 존재하지 않습니다.');
+}
+
 include_once(G5_PLUGIN_PATH.'/jquery-ui/datepicker.php');
 add_stylesheet('<link rel="stylesheet" href="'.THEMA_URL.'/assets/css/center.css">', 0);
 ?>
@@ -26,13 +37,13 @@ add_stylesheet('<link rel="stylesheet" href="'.THEMA_URL.'/assets/css/center.css
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_name"><b>이름</b><strong class="sound_only">필수</strong></label>
                 <div class="col-sm-3">
-                    <input type="text" name="cm_name" value="" id="cm_name" required class="form-control input-sm" minlength="3" maxlength="20">
+                    <input type="text" name="cm_name" value="<?=$cm['cm_name'] ?: ''?>" id="cm_name" required class="form-control input-sm" minlength="3" maxlength="20">
                 </div>
             </div>
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_code"><b>접속코드</b><strong class="sound_only">필수</strong></label>
                 <div class="col-sm-3">
-                    <input type="text" name="cm_code" value="" id="cm_code" required <?php if($w) echo 'readonly' ?> class="form-control input-sm">
+                    <input type="text" name="cm_code" value="<?=$cm['cm_code'] ?: ''?>" id="cm_code" required <?php if($w) echo 'readonly' ?> class="form-control input-sm">
                 </div>
                 <div class="desc_txt">
                     <span id="code_keyup">* 방문기록, 교육정보 열람 시 본인 확인을 위해 필요한 접속코드 입니다.</span>
@@ -41,17 +52,17 @@ add_stylesheet('<link rel="stylesheet" href="'.THEMA_URL.'/assets/css/center.css
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_birth"><b>생년월일</b></label>
                 <div class="col-sm-2">
-                    <input type="text" name="cm_birth" value="" id="cm_birth" class="birthpicker form-control input-sm">
+                    <input type="text" name="cm_birth" value="<?=$cm['cm_birth'] ?: ''?>" id="cm_birth" class="birthpicker form-control input-sm">
                 </div>
             </div>
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label"><b>계약형태</b></label>
                 <div class="col-sm-3">
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_cont" value="1" style="vertical-align: middle; margin: 0 5px 0 0;">정규직
+                        <input type="radio" name="cm_cont" value="1" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_cont'], '1')?>>정규직
                     </label>
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_cont" value="2" style="vertical-align: middle; margin: 0 5px 0 0;">계약직
+                        <input type="radio" name="cm_cont" value="2" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_cont'], '2')?>>계약직
                     </label>
                 </div>
             </div>
@@ -59,10 +70,10 @@ add_stylesheet('<link rel="stylesheet" href="'.THEMA_URL.'/assets/css/center.css
                 <label class="col-sm-2 control-label"><b>분류</b></label>
                 <div class="col-sm-3">
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_type" value="1" style="vertical-align: middle; margin: 0 5px 0 0;">일반직원
+                        <input type="radio" name="cm_type" value="1" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_type'], '1')?>>일반직원
                     </label>
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_type" value="2" style="vertical-align: middle; margin: 0 5px 0 0;">요양보호사
+                        <input type="radio" name="cm_type" value="2" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_type'], '2')?>>요양보호사
                     </label>
                 </div>
             </div>
@@ -70,42 +81,42 @@ add_stylesheet('<link rel="stylesheet" href="'.THEMA_URL.'/assets/css/center.css
                 <label class="col-sm-2 control-label" for="cm_pay"><b>급여</b><strong class="sound_only">필수</strong></label>
                 <div class="col-sm-6">
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_paytype" value="1" style="vertical-align: middle; margin: 0 5px 0 0;">고정
-                        <input type="text" name="cm_pay" id="cm_pay" class="form-control input-sm" style="display: inline-block; width: 100px; margin: 0 5px;">원
+                        <input type="radio" name="cm_paytype" value="1" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_paytype'], '1')?>>고정
+                        <input type="text" name="cm_pay" value="<?=$cm['cm_pay'] ?: ''?>" id="cm_pay" class="form-control input-sm" style="display: inline-block; width: 100px; margin: 0 5px;">원
                     </label>
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_paytype" value="2" style="vertical-align: middle; margin: 0 5px 0 0;">변동
+                        <input type="radio" name="cm_paytype" value="2" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_paytype'], '2')?>>변동
                     </label>
                 </div>
             </div>
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_joindate"><b>입사일</b></label>
                 <div class="col-sm-2">
-                    <input type="text" name="cm_joindate" value="" id="cm_birth" class="datepicker form-control input-sm">
+                    <input type="text" name="cm_joindate" value="<?=$cm['cm_joindate'] ?: ''?>" id="cm_joindate" class="datepicker form-control input-sm">
                 </div>
             </div>
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_retiredate"><b>퇴사일</b></label>
                 <div class="col-sm-6">
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_retired" value="0" style="vertical-align: middle; margin: 0 5px 0 0;">근무중
+                        <input type="radio" name="cm_retired" value="0" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_retired'], '0')?>>근무중
                     </label>
                     <label class="checkbox-inline">
-                        <input type="radio" name="cm_retired" value="1" style="vertical-align: middle; margin: 0 5px 0 0;">퇴사
-                        <input type="text" name="cm_retiredate" id="cm_retiredate" class="datepicker form-control input-sm" style="display: inline-block; width: 140px; margin: 0 5px;">
+                        <input type="radio" name="cm_retired" value="1" style="vertical-align: middle; margin: 0 5px 0 0;" <?=option_array_checked($cm['cm_retired'], '1')?>>퇴사
+                        <input type="text" name="cm_retiredate" value="<?=$cm['cm_retiredate'] ?: ''?>" id="cm_retiredate" class="datepicker form-control input-sm" style="display: inline-block; width: 140px; margin: 0 5px;">
                     </label>
                 </div>
             </div>
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_hp"><b>연락처</b></label>
                 <div class="col-sm-3">
-                    <input type="text" name="cm_hp" value="" id="cm_hp" class="form-control input-sm">
+                    <input type="text" name="cm_hp" value="<?=$cm['cm_hp'] ?: ''?>" id="cm_hp" class="form-control input-sm">
                 </div>
             </div>
             <div class="form-group has-feedback">
                 <label class="col-sm-2 control-label" for="cm_addr"><b>주소</b></label>
                 <div class="col-sm-6">
-                    <input type="text" name="cm_addr" value="" id="cm_addr" class="form-control input-sm">
+                    <input type="text" name="cm_addr" value="<?=$cm['cm_addr'] ?: ''?>" id="cm_addr" class="form-control input-sm">
                 </div>
             </div>
         </div>
