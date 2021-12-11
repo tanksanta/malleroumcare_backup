1c48ef230f297e3eef88042413f77a58cc816afe
39hn <admin@the.gg>
2021-12-11 14:47:19 +0900

    알림톡 삭제

---
+++ www/adm/alimtalk_delete.php
@@ -0,0 +1,33 @@
+<?php
+$sub_menu = '200400';
+include_once('./_common.php');
+
+auth_check($auth[$sub_menu], 'd');
+
+$count = count($_POST['chk']);
+
+if(!$count)
+    alert('삭제할 알림톡 목록을 1개이상 선택해 주세요.');
+
+for($i=0; $i<$count; $i++) {
+    $al_id = $_POST['chk'][$i];
+
+    $sql = "
+        DELETE FROM
+            g5_alimtalk
+        WHERE
+            al_id = '$al_id'
+    ";
+    sql_query($sql);
+
+    $sql = "
+        DELETE FROM
+            g5_alimtalk_member
+        WHERE
+            al_id = '$al_id'
+    ";
+    sql_query($sql);
+}
+
+goto_url('./alimtalk_list.php');
+?>
