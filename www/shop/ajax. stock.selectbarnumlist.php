39e22b03cc83ba4403fdc8eec3c5174d4b4ca6b2
39hn <admin@the.gg>
2022-05-25 13:17:58 +0900

    일부 ajax파일 _common.php 인클루딩 안하는 오류 수정

--- www/shop/ajax. stock.selectbarnumlist.php
+++ www/shop/ajax. stock.selectbarnumlist.php
@@ -1,31 +1,32 @@
 <?php
+include_once('./_common.php');
 
-    $oCurl = curl_init();
-    curl_setopt($oCurl, CURLOPT_PORT, 9901);
-    curl_setopt($oCurl, CURLOPT_URL, EROUMCARE_API_STOCK_SELECT_BARNUM_LIST);
-    curl_setopt($oCurl, CURLOPT_POST, 1);
-    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($oCurl, CURLOPT_POSTFIELDS, json_encode($_POST, JSON_UNESCAPED_UNICODE));
-    curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($oCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-    $res = curl_exec($oCurl);
-    $stockBarList = json_decode($res, true);
-    curl_close($oCurl);
-    // alert($res);
-    // print_r($stockBarList);
-    // echo '<br>';
-    // print_r($stockBarList["errorYN"]);
-    // echo '<br>';
+$oCurl = curl_init();
+curl_setopt($oCurl, CURLOPT_PORT, 9901);
+curl_setopt($oCurl, CURLOPT_URL, EROUMCARE_API_STOCK_SELECT_BARNUM_LIST);
+curl_setopt($oCurl, CURLOPT_POST, 1);
+curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($oCurl, CURLOPT_POSTFIELDS, json_encode($_POST, JSON_UNESCAPED_UNICODE));
+curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($oCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+$res = curl_exec($oCurl);
+$stockBarList = json_decode($res, true);
+curl_close($oCurl);
+// alert($res);
+// print_r($stockBarList);
+// echo '<br>';
+// print_r($stockBarList["errorYN"]);
+// echo '<br>';
+// print_r($stockBarList["message"]);
+// echo '<br>';
+// print_r($stockBarList["data"][0]['prodBarNumList']);
+
+// echo '<br>';
+return false;
+if($stockBarList["errorYN"] == "N"){
+    // print_r($stockBarList["stockBarList"]);
     // print_r($stockBarList["message"]);
-    // echo '<br>';
-    // print_r($stockBarList["data"][0]['prodBarNumList']);
-
-    // echo '<br>';
-    return false;
-    if($stockBarList["errorYN"] == "N"){
-        // print_r($stockBarList["stockBarList"]);
-        // print_r($stockBarList["message"]);
-        // alert($res["message"], "/");
-        // array_push($stoIdList, $res["data"][0]["stoId"]);
-    }
+    // alert($res["message"], "/");
+    // array_push($stoIdList, $res["data"][0]["stoId"]);
+}
 ?>
