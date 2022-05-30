84c596ec656cc9ae4bc70d121a7d56cd0e9c4a26
eroum_admin <tanksanta02@gmail.com>
2022-05-30 17:33:22 +0900

    수급자관련 ajax파일 _common.php 인클루딩 안하는 오류 수정

--- www/shop/ajax.my.recipientSpare.list.php
+++ www/shop/ajax.my.recipientSpare.list.php
@@ -1,16 +1,17 @@
 <?php
+include_once('./_common.php');
 
-	$oCurl = curl_init();
-	curl_setopt($oCurl, CURLOPT_PORT, 9901);
-	curl_setopt($oCurl, CURLOPT_URL, EROUMCARE_API_SPARE_RECIPIENT_SELECTLIST);
-	curl_setopt($oCurl, CURLOPT_POST, 1);
-	curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($oCurl, CURLOPT_POSTFIELDS, json_encode($_POST, JSON_UNESCAPED_UNICODE));
-	curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($oCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-	$res = curl_exec($oCurl);
-	curl_close($oCurl);
+$oCurl = curl_init();
+curl_setopt($oCurl, CURLOPT_PORT, 9901);
+curl_setopt($oCurl, CURLOPT_URL, EROUMCARE_API_SPARE_RECIPIENT_SELECTLIST);
+curl_setopt($oCurl, CURLOPT_POST, 1);
+curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($oCurl, CURLOPT_POSTFIELDS, json_encode($_POST, JSON_UNESCAPED_UNICODE));
+curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($oCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+$res = curl_exec($oCurl);
+curl_close($oCurl);
 
-	echo $res;
+echo $res;
 
 ?>
