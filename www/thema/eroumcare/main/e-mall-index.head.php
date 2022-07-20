9a0390c33a675ccfcc7a03c4463aa68059fc7cc2
dana1250 <73210540+dana1250@users.noreply.github.com>
2022-07-20 17:57:54 +0900

    [Front] Landing page 교체 요청

--- www/thema/eroumcare/main/e-mall-index.head.php
+++ www/thema/eroumcare/main/e-mall-index.head.php
@@ -1,128 +1,22 @@
 <div class="main_top_service_info">
-  <button id="btn_close_service_desc">
-    <i class="fa fa-times" aria-hidden="true"></i>
-  </button>
-  <div class="top_area">
-    <div class="logo"><img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_logo_hd.png"  ></div>
-    <div class="btn_login"><a href="/bbs/login.php">통합관리시스템로그인 ▶</a></div>
-  </div>
-  <div class="service_desc">
-    <div class="txt_area">
-      <p><span class="line"> </span>
-        오직 <span>이로움</span>만의 <span>특별한 관리시스템</span></p>
-      <p>장기요양기관</p>
-      <p>통합관리시스템</p>
-      <p>하단 아이콘에 마우스를 올려보세요.</p>
+    <div class = "main_logo" style="position:relative;  width: 50%; height: 100%; float: left;">
+        <img src="<?=G5_URL?>/thema/eroumcare/assets/img/hd_logo.png" style="width:300px; margin-left: 70px; margin-top: 30px;" >
     </div>
-    <div class="service_wrap">
-      <ul>
-        <li>
-          <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_top_service_icon_01.png"  >
-          복지용구급여
-          <div class="desc_area">
-            <p class="desc_tit">복지용구급여</p>
-            <p>
-              복지용구란 일상생활 · 신체활동의 지원 및 인지 기능의 유지,<br>
-              기능 향상에 필요한 용구를 말합니다.<br><br>
-              
-              본 시스템의 복지용구급여 서비스는<br>
-              수급자 통합 관리 시스템으로 수급자와 복지용구 사업소의<br>
-              연결 기능을 수행합니다.<br><br>
-              
-              외에도 재고관리, 주문과 동시에 수급자 계약 체결 기능,<br>
-              공급계약서, 제공기록지등의 문서 양식 전자서식 도입,<br>
-              건강보험공단 청구 시 검증 기능 등을<br><br>
-              
-              제공하여 사업소의 업무 효율성을 높여줍니다.   
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_top_service_icon_02.png"  >
-          방문급여
-          <div class="desc_area">
-            <p class="desc_tit">방문급여</p>
-            <p>
-              방문급여란 방문요양, 방문목욕, 방문간호 등의 서비스가 제공되며,<br>
-              장기요양요원이나 간호사가 수급자의 가정 등을 방문하여<br>
-              신체 활동 및 가사 활동, 진료의 보조등을 지원하는 장기요양 급여입니다.  <br><br>
-              
-              본 시스템에서 방문급여 서비스는<br>
-              이용자관리/건강관리/종결 및 사후관리 등의 방문 급여시설의<br>
-              업무 서비스를 지원합니다.
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_top_service_icon_03.png"  >
-          주야간보호급여
-          <div class="desc_area">
-            <p class="desc_tit">주야간보호급여</p>
-            <p>
-              주야간보호급여란 수급자를 하루 중 일정한 시간동안 장기요양기관에<br>
-              보호하며 신체활동 지원 및 심신기능의 유지 · 향상을 위한<br>
-              교육 · 훈련 등을 제공하는 장기 요양 급여를 말합니다.<br><br>
-              
-              본 시스템의 주야간보호급여 서비스는<br>
-              이동/목욕/급식/간호/기능회복훈련/치매관리 등의 주야간 보호센터의<br>
-              업무를 쉽고 간편하게 할 수 있도록 지원하는 기능을 제공합니다. 
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_top_service_icon_04.png"  >
-          시설급여
-          <div class="desc_area">
-            <p class="desc_tit">시설급여</p>
-            <p>
-              시설급여란 노인요양시설, 노인요양공동생활 가정 등에<br>
-              장기간 입소하여, 신체활동 지원 및 심신기능유지 향상을 위한<br>
-              교육 훈련 등을 제공합니다.<br><br>
-               
-              * 노인요양시설의 입소정원  10명 이상<br>
-              * 노인요양공동생활가정의 입소정원: 5~9명 
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_top_service_icon_05.png"  >
-          재무회계
-          <div class="desc_area">
-            <p class="desc_tit">재무회계</p>
-            <p>
-              복지용구란 일상생활 · 신체활동의 지원 및 인지 기능의 유지,<br>
-              기능 향상에 필요한 용구를 말합니다.<br><br>
-              
-              본 시스템의 복지용구급여 서비스는<br>
-              수급자 통합 관리 시스템으로 수급자와 복지용구 사업소의<br>
-              연결 기능을 수행합니다.<br><br>
-              
-              외에도 재고관리, 주문과 동시에 수급자 계약 체결 기능,<br>
-              공급계약서, 제공기록지등의 문서 양식 전자서식 도입,<br>
-              건강보험공단 청구 시 검증 기능 등을<br><br>
-              
-              제공하여 사업소의 업무 효율성을 높여줍니다.   
-            </p>
-          </div>
-        </li>
-        <li>
-          <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_top_service_icon_06.png"  >
-          시니어타운
-          <div class="desc_area">
-            <p class="desc_tit">시니어타운</p>
-            <p>
-              본 시스템의 시니어타운 서비스는 시니어를 위한<br>
-              토탈 케어 서비스, 의식주 및 돌봄, <br>
-              각종 정보/어르신 일자리안내/노인용품/중고장터 등의<br>
-              기능을 제공하여 줍니다. 
-
-            </p>
-          </div>
-        </li>
-      </ul>
+    <div style="position:relative; width: 50%; height: 100%; background-color: rgba(255,255,255,0.5); float: left;justify-content: center;">
+        <div style="position:relative; width: 100%; height: 70%; float: top;">
+            <div class="txt_area" style="position:absolute; bottom:0px; width: 90%; float:left; text-align: right;padding:0 20px; margin-right:100px;">
+                <p style="font-weight:500;font-size:70px;color:#27140c;line-height: 60px;margin-bottom: 20px;">장기요양기관</p>
+                <p style="color:#e86b19; font-weight:bold;font-size:70px;line-height: 60px;">통합관리시스템</p>
+                <p style="color:#27140c;"><span class="line"style="content: ' ';width:100px;background: #27140c;height:5px;display:inline-block;margin-top: 30px; margin-bottom: 30px;"> </span> </p>
+                <p style="font-weight: 600; font-size: 25px;color:#27140c; padding:10px;margin-bottom: 3px;">이로움만의 장기요양기관 통합관리 시스템으로</p>
+                <p style="font-weight: 600; font-size: 25px;color:#27140c; padding:10px;margin-bottom: 3px;">모든 것을 쉽고 편하게 관리해보세요</p>
+                <a href="/bbs/login.php">
+                    <p style="margin-top: 80px; font-size: 35px; background-color: #0c0c0c; font-weight:600; display: inline-block; padding: 20px 20px 20px 20px; border: 3px solid #0c0c0c; margin-left: 15px; color: #F5F5F5;">로그인 <span style="color: #e86b19; font-weight: bold;">⇀</span></p>
+                </a>
+                <a href="/bbs/register.php">
+                    <p style="margin-top: 80px; font-size: 35px; background-color: #F5F5F5; font-weight:600; display: inline-block; padding: 20px 20px 20px 20px; border: 3px solid #0c0c0c; margin-left: 15px;">회원가입 <span style="font-weight: bold;">⇀</span></p>
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="service_footer">
-    이로움만의 장기요양기관 통합관리시스템으로 모든 것을 쉽고 편하게 관리해보세요.
-  </div>
 </div>
--- www/thema/eroumcare/main/e-mall-index.php
+++ www/thema/eroumcare/main/e-mall-index.php
@@ -3,11 +3,43 @@
   if(!defined("_GNUBOARD_")) exit;
 
 ?>
+<div class="main_content" style="width: 100%; height: 100%;">
+    <div class="service_content">
+        <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_content.png" style="width: 100%;" >
+    </div>
+
+    <div class="main_top_wrap" style="width: 100%; background-color: #F5F5F5;">
+        <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_code1.png" style="width: 60%;" >
+        <div class="main_slider">
+            <div class="content_slide">
+                <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_01.png" style="width: 100%;" >
+            </div>
+            <div class="content_slide">
+                <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_02.png" style="width: 100%;" >
+            </div>
+            <div class="content_slide">
+                <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_03.png" style="width: 100%;" >
+            </div>
+            <div class="content_slide">
+                <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_04.png" style="width: 100%;" >
+            </div>
+        </div>
+    </div>
+
+    <div class="service_offer">
+        <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_offer.png" style="width: 100%;" >
+    </div>
+
+    <div class="service_infomation" style="width: 100%;">
+        <img src="<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_info.png" style="width: 100%;" >
+    </div>
+</div>
+
 
 <style>
 .main_slider {
   max-width: 1400px;
-  margin: 30px auto 20px auto;
+  margin: auto;
 }
 
 .main_top_wrap .slick-prev,
@@ -30,24 +62,24 @@
     background: transparent;
 }
 .main_top_wrap .slick-prev {
-  top: 150px;
-  left: -50px;
+  top: 250px;
+  left: 15px;
 }
 .main_top_wrap .slick-next {
-  top: 150px;
-  right: -50px;
+  top: 250px;
+  right: 15px;
 }
 
 .main_top_wrap .slick-prev:before,
 .main_top_wrap .slick-next:before {
   display: inline-block;
   font: normal normal normal 14px/1 FontAwesome;
-  font-size: 80px;
+  font-size: 50px;
   line-height: 1;
   text-rendering: auto;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #979c94;
+  color: rgba(255,255,255,0);
 }
 .main_top_wrap .slick-prev:before {
   content: "\f104";
@@ -56,20 +88,10 @@
   content: "\f105";
 }
 
-.main_top_wrap .slick-dots { display: none !important; width: 100%; padding: 0; margin: 0; list-style: none; text-align: center; }
-.main_top_wrap .slick-dots li {	position: relative;	display: inline-block;	width: 20px;	height: 20px;	margin: 0 5px;	padding: 0;	cursor: pointer; }
-.main_top_wrap .slick-dots li button { font-size: 0; line-height: 0; display: block; width: 20px; height: 20px; padding: 5px; cursor: pointer; color: transparent; border: 0; outline: none; background: transparent; }
-.main_top_wrap .slick-dots li button:hover, .item_image_slider .slick-dots li button:focus { outline: none; }
-.main_top_wrap .slick-dots li button:before { font-family: 'slick'; font-size: 6px; line-height: 20px; position: absolute; top: 0; left: 0; width: 20px; height: 20px; content: '•'; text-align: center; color: #e1e1e1; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; }
-.main_top_wrap .slick-dots li.slick-active button:before { color: #737373; }
-
 @media (max-width: 960px) {
   .main_top_wrap .slick-prev,
   .main_top_wrap .slick-next {
     display: none !important;
-  }
-  .main_top_wrap .slick-dots {
-    display: block !important;
   }
 }
 </style>
@@ -85,58 +107,11 @@
         $('.btn_top_scroll').css( "display", "none" );
         $('body').css( "padding-top", "0px" );
     });
-</script>
 
-
-<div class="main_top_wrap">
-	<div class="main_slider_tit">
-		<span>복지용구급여</span> 주요기능
-	</div>
-  <div class="main_slider_nav">
-    <div class="active" data-slide="0">온라인계약</div>
-    <div data-slide="1">청구관리</div>
-    <div data-slide="2">수급자관리</div>
-    <div data-slide="3">재고관리</div>
-  </div>
-  <div class="main_slider">
-    <div class="main_slide">
-    <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_service_info_01.png"  >
-      <div class="main_slide_desc">
-        <h2>편리한 온라인 전자계약</h2>
-        <div class="desc">복지용구 사업소 업무를 쉽고 간편하게</div>
-      </div>
-      
-    </div>
-    <div class="main_slide">
-      <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_service_info_02.png" >
-      <div class="main_slide_desc">
-        <h2>월별 청구관리 및 간편검증</h2>
-        <div class="desc">실시간 수급자 별 청구금액 확인</div>
-      </div>
-    </div>
-    <div class="main_slide">
-      <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_service_info_03.png"  >
-      <div class="main_slide_desc">
-        <h2>간편관리 및 신규 수급자 추천</h2>
-        <div class="desc">쉽고 간편한 수급자 통합관리 시스템</div>
-      </div>
-    </div>
-    <div class="main_slide">
-      <img src="<?=G5_URL?>/thema/eroumcare/assets/img/main_service_info_04.png" >
-      <div class="main_slide_desc">
-        <h2>판매/대여(소독관리) 간편 관리</h2>
-        <div class="desc">현재 보유한 재고를 일괄업로드 후 관리</div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
 $(function() {
   $('.main_slider').slick({
     autoplay: true,
-    autoplaySpeed: 6000,
-    dots: true
+    autoplaySpeed: 6000
   });
 
   $('.main_slider_nav > div').on('click', function() {
