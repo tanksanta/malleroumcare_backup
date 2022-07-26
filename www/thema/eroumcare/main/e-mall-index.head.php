a5c6a9661472ac0d8a6a3f1ab51fc8af9884418c
dana1250 <73210540+dana1250@users.noreply.github.com>
2022-07-26 14:00:10 +0900

    [Front] Landing page 교체 요청-헤더 교체

--- www/thema/eroumcare/main/e-mall-index.head.php
+++ www/thema/eroumcare/main/e-mall-index.head.php
@@ -1,22 +1,71 @@
-<div class="main_top_service_info">
-    <div class = "main_logo" style="position:relative;  width: 50%; height: 100%; float: left;">
-        <img src="<?=G5_URL?>/thema/eroumcare/assets/img/hd_logo.png" style="width:300px; margin-left: 70px; margin-top: 30px;" >
-    </div>
-    <div style="position:relative; width: 50%; height: 100%; background-color: rgba(255,255,255,0.5); float: left;justify-content: center;">
-        <div style="position:relative; width: 100%; height: 70%; float: top;">
-            <div class="txt_area" style="position:absolute; bottom:0px; width: 90%; float:left; text-align: right;padding:0 20px; margin-right:100px;">
-                <p style="font-weight:500;font-size:70px;color:#27140c;line-height: 60px;margin-bottom: 20px;">장기요양기관</p>
-                <p style="color:#e86b19; font-weight:bold;font-size:70px;line-height: 60px;">통합관리시스템</p>
-                <p style="color:#27140c;"><span class="line"style="content: ' ';width:100px;background: #27140c;height:5px;display:inline-block;margin-top: 30px; margin-bottom: 30px;"> </span> </p>
-                <p style="font-weight: 600; font-size: 25px;color:#27140c; padding:10px;margin-bottom: 3px;">이로움만의 장기요양기관 통합관리 시스템으로</p>
-                <p style="font-weight: 600; font-size: 25px;color:#27140c; padding:10px;margin-bottom: 3px;">모든 것을 쉽고 편하게 관리해보세요</p>
-                <a href="/bbs/login.php">
-                    <p style="margin-top: 80px; font-size: 35px; background-color: #0c0c0c; font-weight:600; display: inline-block; padding: 20px 20px 20px 20px; border: 3px solid #0c0c0c; margin-left: 15px; color: #F5F5F5;">로그인 <span style="color: #e86b19; font-weight: bold;">⇀</span></p>
-                </a>
-                <a href="/bbs/register.php">
-                    <p style="margin-top: 80px; font-size: 35px; background-color: #F5F5F5; font-weight:600; display: inline-block; padding: 20px 20px 20px 20px; border: 3px solid #0c0c0c; margin-left: 15px;">회원가입 <span style="font-weight: bold;">⇀</span></p>
-                </a>
-            </div>
-        </div>
+<div class="service_top">
+    <div class="hd_logo"></div>
+    <div class="half_patition">
+        <p>장기요양기관</p>
+        <p>통합관리시스템</p>
+        <p><span class="line"> </span> </p>
+        <p>이로움만의 장기요양기관 통합관리시스템으로<br>모든 것을 쉽고 편하게 관리해보세요</p>
+        <p><a href="/bbs/login.php">로그인 <span>⇀</span></a></p>
+        <p><a href="/bbs/register.php">회원가입 <span>⇀</span></a></p>
     </div>
 </div>
+
+<style>
+.service_top {
+  width: 100%;
+  height: 937px;
+  min-height: 200px;
+  background-image: url("<?=G5_URL?>/thema/eroumcare/assets/img/eroumranding_top.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.half_patition {
+  width: 60%;
+  height: 100%;
+  background-color: rgba(255,255,255,0.5);
+  float: right;
+  text-align: right;
+  padding-right: 10%;
+}
+.hd_logo{
+    width: 12%;
+    height: 12%;
+    background-image: url("<?=G5_URL?>/thema/eroumcare/assets/img/hd_logo.png");
+    background-repeat: no-repeat;
+    background-size: contain;
+    position: absolute;
+    margin: 3%;
+}
+
+.half_patition p:first-child {font-weight: bold; font-size: 60px;line-height: 1.5;margin-top: 15%;}
+.half_patition p:nth-child(2) {color:#e86b19; font-weight: bold; font-size: 60px;line-height: 1.5;}
+.half_patition p:nth-child(3) .line {content: ' ';width:15%;background: #27140c;height:8px;margin-top:5%;display:inline-block;}
+.half_patition p:nth-child(4) {font-weight:500; margin-top:5%; font-size: 25px;line-height: 2;}
+.half_patition p:nth-child(5) {margin-top: 10%; font-size: 30px; background-color: #0c0c0c; font-weight:550; display: inline-block; padding: 20px 25px 20px 25px; border: 3px solid #0c0c0c; margin-left: 8%; color: #F5F5F5;}
+.half_patition p:nth-child(5) span{color: #e86b19; font-weight:1000;}
+.half_patition p:nth-child(6) {margin-top: 10%; font-size: 30px; background-color: #F5F5F5; font-weight:550; display: inline-block; padding: 20px 25px 20px 25px; border: 3px solid #0c0c0c; margin-left: 8%; color: #0c0c0c;}
+.half_patition p:nth-child(6) span{font-weight:1000;}
+
+@media (max-width: 500px) {
+    .service_top{
+        width: 100%;
+        height: 200px;
+        background-size: contain;
+        position: relative;
+    }
+    .hd_logo{
+        width: 20%;
+        height: 20%;
+    }
+
+    .half_patition p:first-child {font-weight: bold; font-size: 120%;line-height: 1;}
+    .half_patition p:nth-child(2) {color:#e86b19; font-weight: bold;font-size: 120%;line-height: 1;}
+    .half_patition p:nth-child(3) .line {content: ' ';width:15%;background: #27140c;height:2px;display:inline-block;}
+    .half_patition p:nth-child(4) {display: none;}
+    .half_patition p:nth-child(5) {margin-top: 1%; font-size: 10px; background-color: #0c0c0c; font-weight:550; display: inline-block; padding: 2px 4px 2px 4px; border: 1px solid #0c0c0c; margin-left: 3%; color: #F5F5F5;}
+    .half_patition p:nth-child(5) span{color: #e86b19; font-weight:1000;}
+    .half_patition p:nth-child(6) {margin-top: 1%; font-size: 10px; background-color: #F5F5F5; font-weight:550; display: inline-block; padding: 2px 4px 2px 4px; border: 1px solid #0c0c0c; margin-left: 3%; color: #0c0c0c;}
+    .half_patition p:nth-child(6) span{font-weight:1000;}
+}
+</style>
