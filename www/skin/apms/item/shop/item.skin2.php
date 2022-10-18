056d0cc5e125be8221c543af57bc6f967125780a
zziny0811 <zziny0811@thkc.co.kr>
2022-10-18 13:59:32 +0900

    [사업소]보유재고관리 신규상품주문 오류수정&개선

--- www/skin/apms/item/shop/item.skin2.php
+++ www/skin/apms/item/shop/item.skin2.php
@@ -16,6 +16,9 @@
 			<input type="hidden" name="it_msg3[]" value="<?php echo $it['pt_msg3']; ?>">
 			<input type="hidden" name="sw_direct" value="1">
 			<input type="hidden" name="url">
+			<input type="hidden" id="it_buy_min_qty" value="<?php echo $it['it_buy_min_qty']; ?>">
+			<input type="hidden" id="it_buy_max_qty" value="<?php echo $it['it_buy_max_qty']; ?>">
+			<input type="hidden" id="it_buy_inc_qty" value="<?php echo $it['it_buy_inc_qty']; ?>">
 
             <script>
                 var sc_price_info;
