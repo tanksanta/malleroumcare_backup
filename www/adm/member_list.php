3f29caf85945c83564b10a3f31df1b0010d493a7
root <root@localhost.localdomain>
2022-10-18 01:16:06 -0400

    commit test

--- www/adm/member_list.php
+++ www/adm/member_list.php
@@ -12,6 +12,7 @@ if(! preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $to_u
 
 $sql_common = " from {$g5['member_table']} ";
 
+// test commit
 //영업담당자 추가
 $flag_m=false;
 if($sfl=="mb_manager"){
