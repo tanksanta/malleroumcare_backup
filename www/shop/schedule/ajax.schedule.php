cb76d52a129b71c8e3993aa3b1c3143c08e1c626
iamiks <rmstjr1030@naver.com>
2022-11-07 18:46:53 +0900

    Fix : API 로직 재작성

--- www/shop/schedule/ajax.schedule.php
+++ www/shop/schedule/ajax.schedule.php
@@ -13,19 +13,19 @@ $code = 201;
 if ($check_sum) {
     // 이미 생성 된 일정이 있는 경우
 
-    if(!$_POST['partner_manager_mb_id']) {
+    if($_POST['partner_manager_mb_id']) {
         // 담당자 지정인 경우
-        $res = update_partner_install_schedule_partner_by_ob_id_and_ct_id($_POST['od_id'], $_POST['ct_id'], $_POST['partner_manager_mb_id']);
+        $res = update_partner_install_schedule_partner_by_ob_id($_POST['od_id'], $_POST['partner_manager_mb_id']);
         if ($res) $code = 200;
     }
 
-    if(!$_POST['status']) {
+    if($_POST['status']) {
         // 상태 변경인 경우
         $res = update_partner_install_schedule_status_by_ob_id_and_ct_id($_POST['od_id'], $_POST['ct_id'], $_POST['status']);
         if ($res) $code = 200;
     }
     
-    if (!$_POST['delivery_date'] && !$_POST['delivery_datetime']) {
+    if ($_POST['delivery_date'] && $_POST['delivery_datetime']) {
         // 출고예정일 변경인 경우
         $res = update_partner_install_schedule_delivery_date_and_delivery_datetime_by_ob_id_and_ct_id($_POST['od_id'], $_POST['ct_id'], $_POST['delivery_date'], $_POST['delivery_datetime']);
         if ($res) $code = 200;
@@ -36,24 +36,24 @@ if ($check_sum) {
     $res = create_partner_install_schedule('생성', $_POST['od_id']);
     if (!$res) json_response(400, '에러');
     
-    if(!$_POST['partner_manager_mb_id']) {
+    if($_POST['partner_manager_mb_id']) {
         // 담당자 지정인 경우
-        $res = update_partner_install_schedule_partner_by_ob_id_and_ct_id($_POST['od_id'], $_POST['ct_id'], $_POST['partner_manager_mb_id']);
+        $res = update_partner_install_schedule_partner_by_ob_id($_POST['od_id'], $_POST['partner_manager_mb_id']);
         if ($res) $code = 200;
     }
 
-    if(!$_POST['status']) {
+    if($_POST['status']) {
         // 상태 변경인 경우
         $res = update_partner_install_schedule_status_by_ob_id_and_ct_id($_POST['od_id'], $_POST['ct_id'], $_POST['status']);
         if ($res) $code = 200;
     }
     
-    if (!$_POST['delivery_date'] && !$_POST['delivery_datetime']) {
+    if ($_POST['delivery_date'] && $_POST['delivery_datetime']) {
         // 출고예정일 변경인 경우
         $res = update_partner_install_schedule_delivery_date_and_delivery_datetime_by_ob_id_and_ct_id($_POST['od_id'], $_POST['ct_id'], $_POST['delivery_date'], $_POST['delivery_datetime']);
         if ($res) $code = 200;
     }
 }
-if ($res) json_response(code, 'OK', $res);
+if ($res) json_response($code, 'OK', $res);
 else json_response(400, '유효하지않은 요청입니다.');
 ?>
