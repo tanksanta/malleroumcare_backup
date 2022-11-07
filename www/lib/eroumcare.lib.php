a19c88a321ac0f197f3456d311becd7497a11269
iamiks <rmstjr1030@naver.com>
2022-11-07 18:42:03 +0900

    Fix : update_partner_install_schedule_partner_by_ob_id  API 수정
    - `update_partner_install_schedule_partner_by_ob_id_and_ct_id` 에서 `update_partner_install_schedule_partner_by_ob_id`으로 함수명 수정
    - 조건문 수정

--- www/lib/eroumcare.lib.php
+++ www/lib/eroumcare.lib.php
@@ -2934,16 +2934,15 @@ function update_partner_install_schedule_delivery_date_and_delivery_datetime_by_
  * 마지막 수정일자 : 2022-11-07
  * 설명 : 설치파트너 매니저 일정 담당자 지정
  * @param integer $od_id
- * @param integer $ct_id
  * @param string $partner_manager_mb_id
  * @return boolean 
  */
-function update_partner_install_schedule_partner_by_ob_id_and_ct_id($od_id, $ct_id, $partner_manager_mb_id) {
+function update_partner_install_schedule_partner_by_ob_id($od_id, $partner_manager_mb_id) {
   $sql = "SELECT mb_id, mb_name, mb_manager FROM g5_member WHERE mb_id = '$partner_manager_mb_id';";
   $partner = sql_fetch($sql);
   if ($partner == null) return false;
   
-  $sql = "UPDATE `partner_inst_sts` SET partner_mb_id = '".$partner["mb_manager"]."', partner_manager_mb_id = '".$partner["mb_id"]."', partner_manager_mb_name = '".$partner["mb_name"]."' WHERE od_id = $od_id AND ct_id = $ct_id";
+  $sql = "UPDATE `partner_inst_sts` SET partner_mb_id = '".$partner["mb_manager"]."', partner_manager_mb_id = '".$partner["mb_id"]."', partner_manager_mb_name = '".$partner["mb_name"]."' WHERE od_id = $od_id;";
   return sql_query($sql);
 }
 
