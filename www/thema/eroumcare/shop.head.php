86edc009a098bb67a5e0a63dab42d538264c48c8
dana1250 <73210540+dana1250@users.noreply.github.com>
2022-11-09 16:46:46 +0900

    [마케팅팀] 침대 렌탈프로그램 런칭(11/14)을 위한 상시배너 및 상세 페이지 변경

--- www/thema/eroumcare/shop.head.php
+++ www/thema/eroumcare/shop.head.php
@@ -632,6 +632,11 @@ if($is_main && !$is_member) {
                 </a>
               </li>
               <li>
+                <a href="/bbs/board.php?bo_table=rental">
+                  이로움렌탈
+                </a>
+              </li>
+              <li>
                 <a href="/bbs/board.php?bo_table=event">
                   이벤트
                 </a>
@@ -748,6 +753,11 @@ if($is_main && !$is_member) {
                 </a>
               </li>
               <li>
+                <a href="/bbs/board.php?bo_table=rental">
+                  이로운렌탈
+                </a>
+              </li>
+              <li>
                 <a href="/bbs/board.php?bo_table=event">
                   이벤트
                 </a>
@@ -792,6 +802,9 @@ if($is_main && !$is_member) {
 
           <?php if ($member['mb_type'] !== 'normal') { ?>
           <div class="btn_info_area">
+            <a href="/bbs/board.php?bo_table=rental">
+              <img src="<?=THEMA_URL?>/assets/img/btn_rental.jpg" alt="렌탈 신청 게시판 안내" />
+            </a>
             <a href="/bbs/board.php?bo_table=sample">
               <img src="<?=THEMA_URL?>/assets/img/btn_free_sample.jpg" alt="무료샘플 신청 게시판 안내" />
             </a>
