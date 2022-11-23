a7f5db40d1fd42a81867c49d3f085f53a3263313
iamiks <rmstjr1030@naver.com>
2022-11-23 13:26:08 +0900

    [파트너]일정관리 기능개발 - 6

--- www/shop/schedule/calendar.php
+++ www/shop/schedule/calendar.php
@@ -443,7 +443,7 @@ include_once("./_common.php");
                         <button
                             class="border rounded-lg px-8 py-1 flex justify-center items-center text-lg bg-blue-500 text-white font-bold transition-colors duration-300"
                             type="button"
-                            @click="if (valueInModal === '') { alter('담당자를 선택해주세요.'); } else { reload = req(calcDaysByMonth(), mb_type, valueInModal); refetch(); valueInModal = ''; showModal = false; }"
+                            @click="if (valueInModal === '' || valueInModal === null) { alert('담당자를 선택해주세요.'); } else { reload = req(calcDaysByMonth(), mb_type, valueInModal); refetch(); valueInModal = ''; showModal = false; }"
                             x-text="'등록'"></button>
                     </div>
                 </div>
--- www/shop/schedule/m_calendar.php
+++ www/shop/schedule/m_calendar.php
@@ -296,7 +296,7 @@ include_once("./_common.php");
                         <button
                             class="border rounded-lg px-8 py-1 flex justify-center items-center text-lg bg-blue-500 text-white font-bold transition-colors duration-300"
                             type="button"
-                            @click="if (valueInModal === '') { alter('담당자를 선택해주세요.'); } else { reload = req(calcDaysByMonth(), mb_type, valueInModal); window.location.reload(); valueInModal = ''; showModal = false; scheduleInit(); }"
+                            @click="if (valueInModal === '' || valueInModal === null) { alert('담당자를 선택해주세요.'); } else { reload = req(calcDaysByMonth(), mb_type, valueInModal); window.location.reload(); valueInModal = ''; showModal = false; scheduleInit(); }"
                             x-text="'등록'"></button>
                     </div>
                 </div>
