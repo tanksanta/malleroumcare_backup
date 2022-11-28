e41de5ed56e3332288b440876ffd896a2cc712f0
dana1250 <73210540+dana1250@users.noreply.github.com>
2022-11-28 18:07:37 +0900

    쿠폰관리< 쿠폰추가의 회원일괄등록 레이어팝업 추가요청 - 1

--- www/adm/shop_admin/ajax.couponmember.php
+++ www/adm/shop_admin/ajax.couponmember.php
@@ -8,40 +8,43 @@ if($auth_check)
 
 $sql_common = " from {$g5['member_table']} ";
 $sql_where = " where mb_id <> '{$config['cf_admin']}' and mb_leave_date = '' and mb_intercept_date ='' ";
+
 if($keyword) { // $mb_name => $keyword로 변경
   $keyword = preg_replace('/\!\?\*$#<>()\[\]\{\}/i', '', strip_tags($keyword));
   $sql_where .= " and (mb_name like '%".sql_real_escape_string($keyword)."%' or mb_id like '%".sql_real_escape_string($keyword)."%' )"; // mb_id와 mb_name 동시 검색
 }
 
-if($page = "batchReg" && count($mbIdList)){
+if($page == "batchReg" && count($mbIdList)){
   $sql_where .= " and ("; // mb_id 검색(일괄등록)
   foreach ($mbIdList as $index => $mbId){
     $sql_where .= "mb_id = '".sql_real_escape_string($mbId)."' "; // mb_id 검색(일괄등록)
     if ($index != array_key_last($mbIdList)) { $sql_where .= "or "; }
   }
   $sql_where .= ")"; // mb_id 검색(일괄등록)
+  $sql_limit = "";
+} else { //일괄등록은 페이징이 필요없음
+  // 테이블의 전체 레코드수만 얻음
+  $sql = " select count(*) as cnt " . $sql_common . $sql_where;
+  $row = sql_fetch($sql);
+  $total_count = $row['cnt'];
+
+  $rows = $config['cf_page_rows'];
+  $total_page  = ceil($total_count / $rows);  // 전체 페이지 계산
+  if ($page < 1) { $page = 1; } // 페이지가 없으면 첫 페이지 (1 페이지)
+  $from_record = ($page - 1) * $rows; // 시작 열을 구함
+  $sql_limit = "limit $from_record, $rows ";
 }
-
-// 테이블의 전체 레코드수만 얻음
-$sql = " select count(*) as cnt " . $sql_common . $sql_where;
-$row = sql_fetch($sql);
-$total_count = $row['cnt'];
-
-$rows = $config['cf_page_rows'];
-$total_page  = ceil($total_count / $rows);  // 전체 페이지 계산
-if ($page < 1) { $page = 1; } // 페이지가 없으면 첫 페이지 (1 페이지)
-$from_record = ($page - 1) * $rows; // 시작 열을 구함
 
 $sql = " select mb_id, mb_name
             $sql_common
             $sql_where
             order by mb_id
-            limit $from_record, $rows ";
+            $sql_limit ";
 $result = sql_query($sql);
 
 $check = [];
 $uncheck = [];
-if($page = "batchReg" && count($mbIdList)){
+if($page == "batchReg" && count($mbIdList)){
   for($i=0; $row=sql_fetch_array($result); $i++) {
     if(in_array($row['mb_id'], $mbIdList)){
       $check[] = $row['mb_id'];
