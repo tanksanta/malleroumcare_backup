ae207c48363a8f005f5a6c0673a50d98fe8d60ee
eroum_admin <tanksanta02@gmail.com>
2023-03-03 16:11:23 +0900

    [20230303] 카탈로그 요청 버튼 생성 - 새창

--- www/thema/eroumcare/shop.head.php
+++ www/thema/eroumcare/shop.head.php
@@ -889,7 +889,7 @@ if($is_main && !$is_member) {
 
           <?php if ($member['mb_type'] !== 'normal') { ?>
           <div class="btn_info_area">
-            <a href="https://forms.gle/5zr5u4aFX4vbjrdT9">
+            <a href="https://forms.gle/5zr5u4aFX4vbjrdT9" target='_blank'>
               <img src="<?=THEMA_URL?>/assets/img/btn_request_catalog.jpg" alt="카탈로그 신청 베너" />
             </a>
             <a href="/bbs/board.php?bo_table=rental">
