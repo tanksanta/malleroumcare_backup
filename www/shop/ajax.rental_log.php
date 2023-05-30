f9bc0da03d5750fb9fbbf3dbfd101ff84c6070fe
admin <tanksanta02@gmail.com>
2023-05-30 11:00:25 +0900

    [20230530]사업소 보유재고상품 수급자 선택 후 계약 시 판매완료ㆍ대여중 처리가 안되는 현상 : 관련 추가 파일이 동일 주제의 다른 티켓에 함께 commit 됨

--- www/shop/ajax.rental_log.php
+++ www/shop/ajax.rental_log.php
@@ -65,10 +65,14 @@ for($i = 0; $row = sql_fetch_array($result); $i++) {
       $document = '<a href="'.G5_SHOP_URL.'/eform/downloadEform.php?od_id='.$od_id.'">계약서</a>';
     } else {
 		//추가
-		$sql = "SELECT a.penId,a.penNm,HEX(a.dc_id) AS UUID FROM `eform_document` AS a INNER JOIN `eform_document_item` AS b ON a.dc_id = b.dc_id WHERE b.it_barcode='".$_POST['barcode']."' AND a.dc_status='3' AND b.it_date='".$date2."'";
+		$sql = "SELECT a.penId,a.penNm,HEX(a.dc_id) AS UUID,dc_sign_send_datetime FROM `eform_document` AS a INNER JOIN `eform_document_item` AS b ON a.dc_id = b.dc_id WHERE b.it_barcode='".$_POST['barcode']."' AND a.dc_status='3' AND b.it_date='".$date2."'";
 		$rows2 = sql_fetch($sql);
 		if($rows2['UUID']) {
-			$document = '<a href="'.G5_SHOP_URL.'/eform/downloadEform.php?dc_id='.$rows2['UUID'].'">계약서</a>';
+			if($rows2['dc_sign_send_datetime'] == "0000-00-00 00:00:00"){
+				$document = '<a href="'.G5_SHOP_URL.'/eform/downloadEform.php?dc_id='.$rows2['UUID'].'">계약서</a>';
+			}else{
+				$document = '<a href="javascript:;" onClick="mds_download(\''.$rows2["UUID"].'\',\'1\')">계약서</a>';
+			}
 		}else{
 			$document = '';
 		}
