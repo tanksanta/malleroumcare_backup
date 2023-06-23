9af7e5fe346dc48fad19416d6a17ae9e7ed80a66
story.sw <story.sw@thkc.co.kr>
2023-06-23 14:24:32 +0900

    [사업소_간편주문서 생성]배송지 목록 레이어 팝업 개선 - 2  - 요청사항 누락분 추가

--- www/shop/orderaddress.php
+++ www/shop/orderaddress.php
@@ -41,7 +41,8 @@ $sql = " select count(ad_id) as cnt " . $sql_common;
 $row = sql_fetch($sql);
 $total_count = $row['cnt'];
 
-$rows = $config['cf_page_rows'];
+//$rows = $config['cf_page_rows'];
+$rows = 20;
 $total_page  = ceil($total_count / $rows);  // 전체 페이지 계산
 if ($page < 1) { $page = 1; } // 페이지가 없으면 첫 페이지 (1 페이지)
 $from_record = ($page - 1) * $rows; // 시작 열을 구함
