524cbeb3ca130c67c69d3de996f7596126132c82
story.sw <story.sw@thkc.co.kr>
2023-07-10 10:09:04 +0900

    [관리자] 출고리스트 로젠 EDI전송 연락처 수정

--- www/adm/shop_admin/ajax.order.delivery.edi.all.php
+++ www/adm/shop_admin/ajax.order.delivery.edi.all.php
@@ -131,8 +131,8 @@ foreach($carts as $cart) {
     $edi['sndCustNm']		= '이로움';
     $edi['sndCustAddr1']	= '인천광역시 서구 이든1로 21';
     $edi['sndCustAddr2']	= '이로움';
-    $edi['sndTelNo']		= '02-830-1301';
-    $edi['sndHandNo']		= '02-830-1301';
+    $edi['sndTelNo']		= '1533-5088';
+    $edi['sndHandNo']		= '1533-5088';
     
     // 수취인
     $edi['rcvCustNm']		= $cart['od_b_name'];
