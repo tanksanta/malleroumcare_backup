7c5354b320d0449c13b74425776093385c941b41
zziny0811 <zziny0811@thkc.co.kr>
2023-10-10 17:35:32 +0900

    카테고리 추가 및 변경기능 작동 요청 3 - 페이징 오류 수정

--- www/shop/list.php
+++ www/shop/list.php
@@ -208,7 +208,9 @@ if ($page < 1) $page = 1;
 $from_record = ($page - 1) * $item_rows;
 
 // 전체 페이지 계산
-$row2 = sql_fetch(" select count(*) as cnt from `{$g5['g5_shop_item_table']}` where $where ");
+$row2 = sql_fetch(" select count(*) as cnt from `{$g5['g5_shop_item_table']}` as jt
+INNER JOIN g5_shop_category c ON jt.ca_id = c.ca_id AND c.ca_use='1'
+where $where ");
 
 // if selected count is 0 then it means it has searched from the subcategory
 // which is dedicated from previous selection 2022.08.03 by Jake
@@ -229,7 +231,9 @@ $num = $total_count - ($page - 1) * $item_rows;
 
 // 커스텀 인덱스
 if ($sort != 'custom') {
-	$list_sql = "select * from `{$g5['g5_shop_item_table']}` where $where order by $ca_sub_orderby $order_by limit $from_record, $item_rows";
+	$list_sql = "select * from `{$g5['g5_shop_item_table']}` as jt
+	INNER JOIN g5_shop_category c ON jt.ca_id = c.ca_id AND c.ca_use='1'
+	where  $where order by $ca_sub_orderby $order_by limit $from_record, $item_rows";
 } else {
 	$list_sql = "select *
 				 from (select *
