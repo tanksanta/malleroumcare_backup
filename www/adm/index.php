22f12fc2e0b51f7181c38157aedb54adab97ad66
admin <tanksanta02@gmail.com>
2023-11-21 13:31:35 +0900

    [20231121]OECS-8] [관리권한설정] 관리권한 설정 UI

--- www/adm/index.php
+++ www/adm/index.php
@@ -4,8 +4,43 @@ include_once('./_common.php');
 if ($is_admin != 'super')
     alert('최고관리자만 접근 가능합니다.');
 //아미나빌더 설치체크
-if(!isset($config['as_thema'])) { 
+if(!isset($config['as_thema'])) {
 	goto_url(G5_ADMIN_URL.'/apms_admin/apms.admin.php');
+}
+
+// 최초접속링크가 있으면 연결, 아니면 관리자메인(/adm/)으로 이동
+$sql = ("   SELECT 
+                count(*) AS cnt
+                ,entry_link 
+            FROM 
+                g5_auth 
+            WHERE 
+                mb_id = '{$member['mb_id']}' 
+                AND entry_menu = 'y'
+");
+$result = sql_fetch($sql);
+
+
+$_goUrl = "";
+if( $_SERVER['HTTPS'] === "on" ) { $_goUrl = "https://" . $_SERVER['SERVER_NAME']; } else { $_goUrl = "http://" . $_SERVER['SERVER_NAME']; }
+
+if ( ($is_admin == 'super') || $result['cnt'] ) {
+
+    $position = strpos($result['entry_link'], $_SERVER['SERVER_NAME']);
+
+    if( $position !== false ) {
+        $_uri_link = substr($result['entry_link'], $position + strlen($_SERVER['SERVER_NAME']));
+    } else { 
+        $_uri_link = $result['entry_link']; 
+    }
+
+    if( $result['entry_link'] ) { goto_url( $_goUrl . $_uri_link ); exit(); }
+
+} else {
+    
+    alert('관리자만 접근 가능합니다.');
+    goto_url( $_goUrl ); exit();
+
 }
 
 @include_once('./safe_check.php');
